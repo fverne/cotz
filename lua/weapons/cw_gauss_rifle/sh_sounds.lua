c683cb34461da6243f73870920d9a37412f8608e
LtTaylor97 <jtaylor97@live.com>
2020-01-07 12:49:20 -0500

    Reorganization, Remove Physical Bullets

--- lua/weapons/cw_gauss_rifle/sh_sounds.lua
+++
@@ -1,6 +0,0 @@
-CustomizableWeaponry:addFireSound("CW_GAUSS_FIRE", "weapons/gauss/gauss_shoot.ogg", 1, 140, CHAN_STATIC)
-
-CustomizableWeaponry:addReloadSound("CW_GAUSS_MAGOUT", "weapons/gauss/gauss_reload_out.ogg")
-CustomizableWeaponry:addReloadSound("CW_GAUSS_MAGIN", "weapons/gauss/gauss_reload_in.ogg")
-
-CustomizableWeaponry:addReloadSound("CW_GAUSS_DRAW", "weapons/gauss/gauss_draw.ogg")
