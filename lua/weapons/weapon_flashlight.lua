7694050eda214499d1cd2ceca0aca33fffdcacfe
fverne <frederikverne95@gmail.com>
2020-02-13 15:15:33 +0100

    stupid script error

--- lua/weapons/weapon_flashlight.lua
+++ lua/weapons/weapon_flashlight.lua
@@ -92,7 +92,10 @@ end
 function SWEP:Holster( wep )
 	if not IsFirstTimePredicted() then return end
 	self.Owner:AllowFlashlight( true )
-	self.Owner:Flashlight( false )
+	if (SERVER) then
+		self.Owner:Flashlight( false )
+	end
+
 	self.Owner:AllowFlashlight( false )
 	self.Active = false
 return true
