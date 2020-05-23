aa916a5efd85d3926dfc0f28dc5be51ea127e582
fverne <frederikverne95@gmail.com>
2020-05-23 15:33:50 +0200

    crosshair plugin updated to IX standards, fixes item pickup arc not appearing

--- gamemodes/ix_stalker/plugins/crosshair.lua
+++ gamemodes/ix_stalker/plugins/crosshair.lua
@@ -1,65 +1,69 @@
-PLUGIN.name = "Crosshair - STALKER"
-PLUGIN.author = "some faggot"
-PLUGIN.desc = "modified crosshair"
+
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Crosshair"
+PLUGIN.author = "Black Tea"
+PLUGIN.description = "A Crosshair."
 
 if (CLIENT) then
-	local function drawcircle( pos, size, col )
-		local color = col[2]
-		surface.SetDrawColor(color.r, color.g, color.b, color.a)
-		surface.DrawCircle(pos[1] - size/2, pos[2] - size/2, size, size)
+	local crosshairmat = Material("stalker/crosshair.vtf")
 
-		local color = col[1]
-		surface.SetDrawColor(color.r, color.g, color.b, color.a)
-		surface.DrawOutlinedRect(pos[1] - size/2, pos[2] - size/2 , size, size)
-	end
-
-	local function drawdot( pos, size, col )
-		local color = col[2]
-		surface.SetDrawColor(color.r, color.g, color.b, color.a)
-		surface.SetMaterial( Material("stalker/crosshair.vtf") )
+	local function drawdot( pos, size )
+		surface.SetDrawColor(255, 255, 255, 255)
+		surface.SetMaterial( crosshairmat )
 		surface.DrawTexturedRect(pos[1] - size/2, pos[2] - size/2, size, size)
-		
 	end
 
-	local w, h, aimVector, punchAngle, ft, screen, scaleFraction, distance, entity
+	local aimVector, punchAngle, ft, screen, scaleFraction, distance
 	local math_round = math.Round
-	local curGap = 0
 	local curAlpha = 0
 	local maxDistance = 1000 ^ 2
 	local crossSize = 4
-	local crossGap = 0
 	local colors = {color_black}
 	local filter = {}
 
-	function PLUGIN:PostDrawHUD()
-		local client = LocalPlayer()
+	function PLUGIN:DrawCrosshair(x, y, trace)
+		local entity = trace.Entity
+		distance = trace.StartPos:DistToSqr(trace.HitPos)
+		scaleFraction = 1 - math.Clamp(distance / maxDistance, 0, .5)
+		crossSize = math.Clamp( 35 * scaleFraction, 25, 65)
 		
+		drawdot( {math_round(screen.x), math_round(screen.y)}, crossSize)
+	end
 
-		if (!client:GetChar() or !client:Alive()) then
+	-- luacheck: globals g_ContextMenu
+	function PLUGIN:PostDrawHUD()
+		ix.hud.DrawAll(true)
+
+		local client = LocalPlayer()
+		if (!client:GetCharacter() or !client:Alive()) then
 			return
 		end
 
 		local entity = Entity(client:GetLocalVar("ragdoll", 0))
+
 		if (entity:IsValid()) then
 			return
 		end
-		local wep = client:GetActiveWeapon()
-		if (wep and wep:IsValid() and wep.HUDPaint) then
-			return
-		end
 
-		if (hook.Run("ShouldDrawCrosshair") == false or g_ContextMenu:IsVisible() or ix.gui.characterMenu:IsVisible()) then
-			return
-		end
-		
+		local wep = client:GetActiveWeapon()
+		local bShouldDraw = hook.Run("ShouldDrawCrosshair", client, wep)
+
 		if (client:KeyDown(IN_ATTACK2)) then
 			return
 		end
-		
-		
+
+		if (bShouldDraw == false or !IsValid(wep) /*or wep.DrawCrosshair == false ALWAYS DISPLAY CROSSHAIR*/) then
+			return
+		end
+
+		if (bShouldDraw == false or g_ContextMenu:IsVisible() or
+			(IsValid(ix.gui.characterMenu) and !ix.gui.characterMenu:IsClosing())) then
+			return
+		end
+
 		aimVector = client:EyeAngles()
-		punchAngle = client:GetPunchAngle()
-		w, h = ScrW(), ScrH()
+		punchAngle = client:GetViewPunchAngles()
 		ft = FrameTime()
 		filter = {client}
 
@@ -68,35 +72,23 @@ if (CLIENT) then
 			aimVector = aimVector + vehicle:GetAngles()
 			table.insert(filter, vehicle)
 		end
-		
+
 		local data = {}
 			data.start = client:GetShootPos()
-			data.endpos = data.start + (aimVector + punchAngle):Forward()*65535
+			data.endpos = data.start + (aimVector + punchAngle):Forward() * 65535
 			data.filter = filter
 		local trace = util.TraceLine(data)
 
-		entity = trace.Entity
-		distance = trace.StartPos:DistToSqr(trace.HitPos)
-		scaleFraction = 1 - math.Clamp(distance / maxDistance, 0, .5)
+		local drawTarget = self
+		local drawFunction = self.DrawCrosshair
+
+		-- we'll manually call this since CHudCrosshair is never drawn; checks are already performed
+		if (wep.DoDrawCrosshair) then
+			drawTarget = wep
+			drawFunction = wep.DoDrawCrosshair
+		end
+
 		screen = trace.HitPos:ToScreen()
-		crossSize = math.Clamp( 35 * scaleFraction,25,65)
-		crossGap = 25 * (scaleFraction - (client:IsWepRaised() and 0 or .1))
-
---		if (IsValid(entity) and entity:GetClass() == "nut_item" and 
---			entity:GetPos():DistToSqr(data.start) <= 16384) then
---			crossGap = 0
---			crossSize = 5
---		end
-
-		curGap = Lerp(ft * 2, curGap, crossGap)
-		curAlpha = Lerp(ft * 2, curAlpha, (!client:IsWepRaised() and 255 or 150))
-		curAlpha = hook.Run("GetCrosshairAlpha", curAlpha) or curAlpha
-		colors[2] = Color(255, curAlpha, curAlpha, curAlpha)
-	
-		drawdot( {math_round(screen.x), math_round(screen.y)}, crossSize, colors)
-		--drawdot( {math_round(screen.x + curGap), math_round(screen.y)}, crossSize, colors)
-		--drawdot( {math_round(screen.x - curGap), math_round(screen.y)}, crossSize, colors) 
-		--drawdot( {math_round(screen.x), math_round(screen.y + curGap * .8)}, crossSize, colors) 
-		--drawdot( {math_round(screen.x), math_round(screen.y - curGap * .8)}, crossSize, colors) 
+		drawFunction(drawTarget, screen.x, screen.y, trace)
 	end
 end
