35c78f23c2a21337535f3c74d966e4ca606cbe19
fverne <frederikverne95@gmail.com>
2020-07-06 16:33:20 +0200

    error fix

--- gamemodes/ix_stalker/schema/derma/cl_scoreboard.lua
+++ gamemodes/ix_stalker/schema/derma/cl_scoreboard.lua
@@ -157,7 +157,7 @@ function PANEL:Init()
 
 	self.icon.Paint = function(this, w, h)
 		local client = self.player
-		surface.SetMaterial(Material(client:GetCharacter():GetData("pdaavatar")))
+		surface.SetMaterial(Material(client:GetCharacter():GetData("pdaavatar", "vgui/icons/face_31.png")))
 		surface.SetDrawColor(255, 255, 255)
 		surface.DrawTexturedRect(0, 0, w, h)
 	end
