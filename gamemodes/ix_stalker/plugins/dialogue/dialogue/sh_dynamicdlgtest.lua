070c85c092e0743a39b612154ea1988b424143ba
fagnig <fagnig@autistklassen.dk>
2020-07-14 17:51:42 +0200

    Second pass on dynamicdlg

--- gamemodes/ix_stalker/plugins/dialogue/dialogue/sh_dynamicdlgtest.lua
+++ gamemodes/ix_stalker/plugins/dialogue/dialogue/sh_dynamicdlgtest.lua
@@ -4,7 +4,7 @@ DIALOGUE.addTopic("GREETING", {
 	response = "Welcome to the zone!",
 	options = {
 		"RepairItems", 
-    "GetTask",
+		"GetTask",
 		"GOODBYE"
 	}
 })
@@ -18,28 +18,32 @@ DIALOGUE.addTopic("Return", {
 DIALOGUE.addTopic("RepairItem1", { -- Repair primary weapon
 	statement = "",
 	response = "",
-  preCallback = function(self, client, target)
-		if (CLIENT) then
-      local char = client:GetCharacter()
-      
-      local cost = 1000 -- Repair formula (?)
-      
-      if (char && char:HasMoney(cost)) then
-        char:TakeMoney(cost);
-        local items = client:GetCharacter():GetInventory():GetItems()
-        local foundName = "ERROR"
+	preCallback = function(self, client, target)
+		local char = client:GetCharacter()
+			
+		local cost = 1000 -- Repair formula (?)
+			
+		if (char && char:HasMoney(cost)) then
+			local items = client:GetCharacter():GetInventory():GetItems()
+			local foundItem = "ERROR"
 
-	      for _, item in pairs( items ) do
-				  if item.weaponCategory == "primary" && item:GetData("equip", false) then
-					  foundName = item.name
-            item:SetData("durability", item:GetData("durability", 0) + 20)
-            item:SetData("wear", 100);
-				  end
-        end
-			  self.response = "Okay "..client:GetName()..", I have repaired and cleaned your "..foundName.."."
-      else 
-        self.response = "You don't have "..cost.." "..ix.currency.plural.."!"b
-      end
+			for _, item in pairs( items ) do -- abstract this into library player:GetPrimaryWeapon()
+				if item.weaponCategory == "primary" && item:GetData("equip", false) then
+					foundName = item.name
+					if (SERVER) then
+						item:SetData("durability", math.Clamp(item:GetData("durability", 0) + 20, 0, 100))
+						item:SetData("wear", 100);
+						char:TakeMoney(cost)
+					end
+				end
+			end
+			if (CLIENT) then
+				self.response = "Okay "..client:GetName()..", I have repaired and cleaned your "..foundName.."."
+			end
+		else 
+			if(CLIENT) then
+				self.response = "You don't have "..cost.." "..ix.currency.plural.."!"
+			end
 		end
 	end,
 	options = {
@@ -50,28 +54,32 @@ DIALOGUE.addTopic("RepairItem1", { -- Repair primary weapon
 DIALOGUE.addTopic("RepairItem2", { -- Repair secondary weapon
 	statement = "",
 	response = "",
-  preCallback = function(self, client, target)
-		if (CLIENT) then
-      local char = client:GetCharacter()
+	preCallback = function(self, client, target)
+		local char = client:GetCharacter()
 
-      local cost = 1000 -- Repair formula (?)
-      
-      if (char && char:HasMoney(cost)) then
-        char:TakeMoney(cost);
-        local items = client:GetCharacter():GetInventory():GetItems()
-        local foundName = "ERROR"
+		local cost = 1000 -- Repair formula (?)
+			
+		if (char && char:HasMoney(cost)) then
+			local items = client:GetCharacter():GetInventory():GetItems()
+			local foundName = "ERROR"
 
-	      for _, item in pairs( items ) do
-				  if item.weaponCategory == "Secondary" && item:GetData("equip", false) then
-					  foundName = item.name
-            item:SetData("durability", item:GetData("durability", 0) + 20)
-            item:SetData("wear", 100);
-				  end
-        end
-			  self.response = "Okay "..client:GetName()..", I have repaired and cleaned your "..foundName.."."
-      else 
-        self.response = "You don't have "..cost.." "..ix.currency.plural.."!"
-      end
+			for _, item in pairs( items ) do
+				if item.weaponCategory == "Secondary" && item:GetData("equip", false) then
+					foundName = item.name
+					if (SERVER) then
+					 	item:SetData("durability", math.Clamp(item:GetData("durability", 0) + 20, 0, 100))
+					 	item:SetData("wear", 100);
+						char:TakeMoney(cost)
+					end
+				end
+			end
+			if (CLIENT) then
+			 	self.response = "Okay "..client:GetName()..", I have repaired and cleaned your "..foundName.."."
+			end
+		else 
+			if (CLIENT) then
+			 	self.response = "You don't have "..cost.." "..ix.currency.plural.."!"
+			end
 		end
 	end,
 	options = {
@@ -83,27 +91,27 @@ DIALOGUE.addTopic("RepairItems", {
 	statement = "Can you repair my items?",
 	response = "What would you like me to look at?",
 	options = {
-    "RepairItem1",
-    "RepairItem2"
-  },
-  preCallback = function(self, client, target)
-    if(CLIENT)then
-      self.response = self.response --Set up random phrases in library so the dialogue doesn't get too stale
-    
-      local char = client:GetCharacter()
-		  if (char) then
-		    local items = char:GetInventory():GetItems()
+		"RepairItem1",
+		"RepairItem2"
+	},
+	preCallback = function(self, client, target)
+		if(CLIENT)then
+			self.response = self.response --Set up random phrases in library so the dialogue doesn't get too stale
+		
+			local char = client:GetCharacter()
+			if (char) then
+				local items = char:GetInventory():GetItems()
 
-	      for _, item in pairs( items ) do
-		  		if item.weaponCategory == "primary" && item:GetData("equip", false) then
-		  			DIALOGUE.tree["RepairItem1"].statement = item.name
-		  		end
-          if item.weaponCategory == "secondary" && item:GetData("equip", false) then
-		  			DIALOGUE.tree["RepairItem2"].statement = item.name
-		  		end
-		  	end
-      end
-    end
+				for _, item in pairs( items ) do
+					if item.weaponCategory == "primary" && item:GetData("equip", false) then
+						DIALOGUE.tree["RepairItem1"].statement = item.name
+					end
+					if item.weaponCategory == "secondary" && item:GetData("equip", false) then
+						DIALOGUE.tree["RepairItem2"].statement = item.name
+					end
+				end
+			end
+		end
 	end,
 })
 
@@ -111,7 +119,7 @@ DIALOGUE.addTopic("GetTask", {
 	statement = "Do you have any work for me?",
 	response = "Yes, have a look.",
 	options = {
-		"GOODBYE"
+		"Return"
 	}
 })
 
