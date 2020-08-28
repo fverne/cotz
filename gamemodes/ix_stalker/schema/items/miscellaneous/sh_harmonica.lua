73e8766f1cef433f3eaf7339fd193446258f20d1
fverne <frederikverne95@gmail.com>
2020-08-28 06:35:33 +0200

    added playable harmonica - needs item model and cooldown still

---
+++ gamemodes/ix_stalker/schema/items/miscellaneous/sh_harmonica.lua
@@ -0,0 +1,30 @@
+ITEM.name = "Harmonica"
+ITEM.description = "A playable harmonica."
+ITEM.model = "models/kek1ch/iphone.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.price = 500
+ITEM.img = Material("vgui/hud/harmonica.png")
+
+ITEM.sounds = {
+	Sound("stalkersound/harmonica/harmonica_1.ogg"),
+	Sound("stalkersound/harmonica/harmonica_2.ogg"),
+	Sound("stalkersound/harmonica/harmonica_3.ogg"),
+	Sound("stalkersound/harmonica/harmonica_4.ogg"),
+	Sound("stalkersound/harmonica/harmonica_5.ogg")
+}
+
+ITEM.functions.use = { -- sorry, for name order.
+	name = "Play",
+	icon = "icon16/stalker/sell.png",
+	OnRun = function(item)
+		local soundplay = table.Random(item.sounds)
+		item.player:EmitSound(soundplay)
+		ix.chat.Send(item.player, "iteminternal", "puts their "..item.name.." to their mouths and plays a tune.", false)
+
+		return false
+	end,
+	OnCanRun = function(item)
+		return (!IsValid(item.entity))
+	end
+}
