5d3962db4297eab84e48701f2a15c14afeef0251
fverne <frederikverne95@gmail.com>
2020-11-12 16:05:25 +0100

    perkele

--- gamemodes/ix_stalker/plugins/discord/sh_plugin.lua
+++ gamemodes/ix_stalker/plugins/discord/sh_plugin.lua
@@ -2,4 +2,9 @@ PLUGIN.name = "Discord Integration"
 PLUGIN.author = "verne"
 PLUGIN.description = "Discord integration for PDA chat."
 
-ix.util.IncludeDir("discord_relay")
+ix.util.Include("cl_relay.lua")
+ix.util.Include("sh_discordcolors.lua")
+ix.util.Include("sv_api.lua")
+ix.util.Include("sv_commands.lua")
+ix.util.Include("sv_config.lua")
+ix.util.Include("sv_relay.lua")
