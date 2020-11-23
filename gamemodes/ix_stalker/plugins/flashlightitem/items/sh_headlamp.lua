0b9280ce89002a2f95f943475d73046e6fbc8f1c
fverne <frederikverne95@gmail.com>
2020-11-23 10:40:48 +0100

    headlamp iconcam

--- gamemodes/ix_stalker/plugins/flashlightitem/items/sh_headlamp.lua
+++ gamemodes/ix_stalker/plugins/flashlightitem/items/sh_headlamp.lua
@@ -8,6 +8,13 @@ ITEM.busflag = {"hardware"}
 ITEM.price = 2500
 ITEM.repairCost = ITEM.price/100*1
 
+ITEM.exRender = true
+ITEM.iconCam = {
+	pos = Vector(-0.5, 11, 0),
+	ang = Angle(0, 270, 0),
+	fov = 45,
+}
+
 ITEM:Hook("drop", function(item)
 	item.player:Flashlight(false)
 end)
