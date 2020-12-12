8c7a0b0294c835033f214f47d86316b5ee06d5fb
fagnig <fagnig@autistklassen.dk>
2020-12-13 00:40:35 +0100

    Add first draft of blowout plugin

---
+++ gamemodes/ix_stalker/plugins/blowout/cl_plugin.lua
@@ -0,0 +1,182 @@
+local SoundNames = {
+	"blowout_announce1",
+	"blowout_announce2",
+	"blowout_prehit",
+	"blowout_ambient1",
+	"blowout_ambient2",
+	"blowout_ambient3",
+	"blowout_ambient4",
+	"blowout_ambient5",
+	"blowout_start",
+	"blowout_buildup",
+	"blowout_hit1",
+	"blowout_hit2",
+	"blowout_hit3",
+	"blowout_end",
+	"blowout_posthit"
+}
+
+local SoundPaths = {
+	"hgn/stalker/blowout/barman2.mp3",
+	"blowout/announce2.wav",
+	"stalkersound/blowout_siren.wav",
+	"blowout/ambient1.wav",
+	"blowout/ambient2.wav",
+	"blowout/ambient3.wav",
+	"blowout/ambient4.wav",
+	"blowout/ambient5.wav",
+	"blowout/start.wav",
+	"blowout/buildup.wav",
+	"blowout/hit1.wav",
+	"blowout/hit2.wav",
+	"blowout/hit3.wav",
+	"blowout/end.wav",
+	"hgn/stalker/blowout/barman3.mp3"
+}
+do
+for i=1, #SoundNames do	--Registering custom sound scripts - probably the best way of playing multiple world-sounds on the client...
+	sound.Add( {
+		name = SoundNames[i],
+		channel = CHAN_STATIC,
+		level = 0,
+		sound = SoundPaths[i]
+	} )
+end
+
+for k,v in pairs(SoundPaths) do
+	util.PrecacheSound(v)
+end
+end
+
+-- ALL CLIENTSIDE
+ix.blowout = ix.blowout or {}
+ix.blowout.Flash = 0
+ix.blowout.StartTime = 0
+ix.blowout.Duration = 0
+ix.blowout.BlowoutPhase = 0
+ix.blowout.LastContrast = 0
+ix.blowout.BlowoutSounds = {}
+ix.blowout.BlowoutSounds[1] = "blowout_announce1"
+ix.blowout.BlowoutSounds[2] = "blowout_announce2"
+ix.blowout.BlowoutSounds[3] = "blowout_prehit"
+ix.blowout.BlowoutSounds[4] = "blowout_ambient1"
+ix.blowout.BlowoutSounds[5] = "blowout_ambient2"
+ix.blowout.BlowoutSounds[6] = "blowout_ambient3"
+ix.blowout.BlowoutSounds[7] = "blowout_ambient4"
+ix.blowout.BlowoutSounds[8] = "blowout_ambient5"
+ix.blowout.BlowoutSounds[9] = "blowout_start"
+ix.blowout.BlowoutSounds[10] = "blowout_buildup"
+ix.blowout.BlowoutSounds[11] = "blowout_hit1"
+ix.blowout.BlowoutSounds[12] = "blowout_hit2"
+ix.blowout.BlowoutSounds[13] = "blowout_hit3"
+ix.blowout.BlowoutSounds[14] = "blowout_end"
+ix.blowout.BlowoutSounds[15] = "blowout_posthit"
+ix.blowout.BlowoutSounds[16] = "blowout_posthit"
+
+net.Receive("BlowoutPlaySound", function()
+	local sndtoplay = net.ReadUInt(8)
+
+	local File = ix.blowout.BlowoutSounds[sndtoplay] or ""	--Play the sound
+	if (File != "") then
+		LocalPlayer():EmitSound(File, 500, 100)
+	end
+
+	if sndtoplay >= 11 and sndtoplay <= 13 then
+		ix.blowout.Flash = CurTime()
+	end
+end)
+
+net.Receive("BlowoutChangePhase", function()
+	local phasechange = net.ReadUInt(8)
+	local duration    = net.ReadFloat()
+
+	ix.blowout.BlowoutPhase = phasechange
+	ix.blowout.StartTime = CurTime()
+	ix.blowout.Duration = duration
+
+	if phasechange == 5 then
+		ix.blowout.StartTime = duration -- ???????? for what purpose
+		ix.blowout.Duration = math.max(ix.blowout.StartTime - CurTime(), 0.01)
+	end
+end)
+
+
+local function BlowoutPostProcess()
+	if ix.blowout.BlowoutPhase <= 0 then return end
+	local CTm = CurTime()
+	local tab = {}
+
+	if ix.blowout.BlowoutPhase == 1 then
+		local Var = math.min((CTm - ix.blowout.StartTime) / ix.blowout.Duration, 1)
+
+		tab[ "$pp_colour_addr" ] = 0
+		tab[ "$pp_colour_addg" ] = 0
+		tab[ "$pp_colour_addb" ] = 0
+		tab[ "$pp_colour_brightness" ] = -Var * 0.1
+		tab[ "$pp_colour_contrast" ] = 1 - Var * 0.2
+		tab[ "$pp_colour_colour" ] = 1 - Var * 0.5
+		tab[ "$pp_colour_mulr" ] = 0
+		tab[ "$pp_colour_mulg" ] = 0
+		tab[ "$pp_colour_mulb" ] = 0
+
+	elseif ix.blowout.BlowoutPhase == 2 then --Flashing right before hit, old phase 3
+		local Var = math.min((CTm - ix.blowout.StartTime) / 15,1)
+		local Var2 = Var ^ 2
+		local FlashVar = math.max(1.5 - (CTm - ix.blowout.Flash),0) / 3
+
+		tab[ "$pp_colour_addr" ] = 0
+		tab[ "$pp_colour_addg" ] = -Var * 0.15
+		tab[ "$pp_colour_addb" ] = -Var * 0.25
+		tab[ "$pp_colour_brightness" ] = -0.15 - Var * 0.15 + FlashVar
+		tab[ "$pp_colour_contrast" ] = 1.5 + Var2 * 2
+		tab[ "$pp_colour_colour" ] = 0.3 - Var * 0.3
+		tab[ "$pp_colour_mulr" ] = Var * 0.4
+		tab[ "$pp_colour_mulg" ] = Var * 0.3
+		tab[ "$pp_colour_mulb" ] = 0
+
+		DrawBloom( 1 - Var / 1.5, 1 - Var / 1.5, 10, 10, 3, 1, 1, 1, 1)
+	elseif ix.blowout.BlowoutPhase == 3 then -- Knockout once the blowout hits, old phase 6
+		local Var = math.min((CTm - ix.blowout.StartTime) / 1.8,1)
+		local Var2 = math.sin(Var * math.pi / 2)
+
+		tab[ "$pp_colour_addr" ] = 0.5
+		tab[ "$pp_colour_addg" ] = 0
+		tab[ "$pp_colour_addb" ] = 0
+		tab[ "$pp_colour_brightness" ] = 0.4 - Var * 0.5
+		tab[ "$pp_colour_contrast" ] = 2.1 - Var2 * 2 - TimedCos(0.55,0,0.3,0)
+		tab[ "$pp_colour_colour" ] = 1 - Var * 0.8
+		tab[ "$pp_colour_mulr" ] = 0
+		tab[ "$pp_colour_mulg" ] = 0
+		tab[ "$pp_colour_mulb" ] = 0
+
+	elseif ix.blowout.BlowoutPhase == 4 then -- Waking up after blowout, old phase 7
+		local Var = math.min((CTm - ix.blowout.StartTime) / ix.blowout.Duration,1)
+		local Var2 = math.sin(Var * math.pi / 2)
+
+		tab[ "$pp_colour_addr" ] = (1 - Var) ^ 2 * 0.5
+		tab[ "$pp_colour_addg" ] = 0
+		tab[ "$pp_colour_addb" ] = 0
+		tab[ "$pp_colour_brightness" ] = -0.1
+		tab[ "$pp_colour_contrast" ] = 0.1 + 0.8 * Var - TimedCos(0.55,0,0.3,0) * (1 - Var ^ 2)
+		tab[ "$pp_colour_colour" ] = 0.2 + Var * 0.3
+		tab[ "$pp_colour_mulr" ] = 0
+		tab[ "$pp_colour_mulg" ] = 0
+		tab[ "$pp_colour_mulb" ] = 0
+	elseif ix.blowout.BlowoutPhase == 5 then -- Clearing up after blowout, old phase 2
+		local Var = math.max( math.min((ix.blowout.StartTime - CTm) / ix.blowout.Duration, 1), 0)
+
+		tab[ "$pp_colour_addr" ] = 0
+		tab[ "$pp_colour_addg" ] = 0
+		tab[ "$pp_colour_addb" ] = 0
+		tab[ "$pp_colour_brightness" ] = -Var * 0.1
+		tab[ "$pp_colour_contrast" ] = 1 - Var * 0.2
+		tab[ "$pp_colour_colour" ] = 1 - Var * 0.5
+		tab[ "$pp_colour_mulr" ] = 0
+		tab[ "$pp_colour_mulg" ] = 0
+		tab[ "$pp_colour_mulb" ] = 0
+	end
+
+    DrawColorModify( tab )
+end
+
+hook.Add("RenderScreenspaceEffects", "BlowoutPostProcess", BlowoutPostProcess)
---
+++ gamemodes/ix_stalker/plugins/blowout/sh_plugin.lua
@@ -0,0 +1,238 @@
+local PLUGIN = PLUGIN
+PLUGIN.name = "Blowout"
+PLUGIN.author = "gumlefar"
+PLUGIN.desc = "Blowout addon converted to plugin, and cleaned up a bit"
+
+ix.util.Include("cl_plugin.lua")
+
+-- Register HUD Bars.
+if (SERVER) then
+
+	util.AddNetworkString("BlowoutPlaySound")
+	util.AddNetworkString("BlowoutChangePhase")
+
+	PLUGIN.NextBlowout = CurTime() + 600
+	
+	PLUGIN.NextThink = 0
+
+	PLUGIN.BlowoutVars = PLUGIN.BlowoutVars or {}
+	PLUGIN.BlowoutVars.BlowoutStarted 	= false
+	PLUGIN.BlowoutVars.PrehitTime  		= 0
+	PLUGIN.BlowoutVars.BlastTime   		= 0
+	PLUGIN.BlowoutVars.BuildupTime 		= 0
+	PLUGIN.BlowoutVars.BlowoutHitTime   = 0
+	PLUGIN.BlowoutVars.FinalSoundTime   = 0
+
+	PLUGIN.BlowoutVars.NextAmbientSound = 0
+	PLUGIN.BlowoutVars.NextHitSound 	= 0
+	PLUGIN.BlowoutVars.BlowoutStep 		= 0
+
+	PLUGIN.BlowoutVars.PostHitActionTime= 0
+	PLUGIN.BlowoutVars.BlowoutFinishTime= 0
+
+	function PLUGIN:Think()
+		local CT = CurTime()
+		if CT < self.NextThink then return end
+
+
+		if self.NextBlowout < CurTime() and (!self.BlowoutVars.BlowoutStarted) then
+			self.BlowoutVars.BlowoutStarted = true
+			local dur = 30 --time until blowout hits - 60-90s probably
+
+			self:ChangePhase(1, dur)
+			self.BlowoutVars.PrehitTime  = CT + dur - 6
+			self.BlowoutVars.BlastTime   = CT + dur
+
+			self.BlowoutVars.BlowoutStep = 1
+
+			self:PlaySoundOnClient( "announce1" )
+		end
+
+		if( self.BlowoutVars.BlowoutStarted ) then
+			self.NextThink = CT + 0.25
+
+			if( self.BlowoutVars.PrehitTime < CT and self.BlowoutVars.BlowoutStep < 2 ) then
+				self.BlowoutVars.BlowoutStep = 2
+				self:PlaySoundOnClient( "prehit" )
+				print("Prehit - phase 2")
+			end
+
+			if( self.BlowoutVars.BlastTime < CT and self.BlowoutVars.BlowoutStep < 3 ) then
+				self.BlowoutVars.BlowoutStep = 3
+				self.BlowoutVars.BlowoutHitTime = CT + 18
+				self.BlowoutVars.BuildupTime = CT + 2
+				self.BlowoutVars.FinalSoundTime = CT + 17.25
+				self:PlaySoundOnClient( "start" )
+				self:ChangePhase(2, 0)
+
+				print("Start - phase 3")
+
+				local e=ents.Create("env_shake")	--Shake the world a little
+				e:SetKeyValue( "amplitude", "1")
+				e:SetKeyValue("frequency","100.0")
+				e:SetKeyValue("duration","10")
+				e:SetKeyValue("spawnflags", "5")
+				e:Fire("startshake")
+			end
+
+			if( self.BlowoutVars.BuildupTime < CT and self.BlowoutVars.BlowoutStep == 3 ) then
+				self.BlowoutVars.BlowoutStep = 4
+				self:PlaySoundOnClient( "buildup" )
+
+				print("buildup - phase 4")
+			end
+
+			if( self.BlowoutVars.FinalSoundTime < CT and self.BlowoutVars.BlowoutStep == 4 ) then
+				self.BlowoutVars.BlowoutStep = 5
+				self:PlaySoundOnClient( "end" )
+				print("end - phase 5")
+			end
+
+			if( self.BlowoutVars.BlowoutHitTime < CT and self.BlowoutVars.BlowoutStep == 5 ) then
+				self.BlowoutVars.BlowoutStep = 6
+				for _, v in pairs(player.GetAll()) do
+					if (!self:IsPosSafe(v:GetShootPos())) then
+						v:Kill()
+					end
+
+					
+				end
+				self:ChangePhase(3, 0)
+
+				ix.plugin.list["anomalycontroller"]:cleanAnomalies()
+				ix.plugin.list["anomalycontroller"]:spawnAnomalies()
+				
+				print("killing - phase 6")
+				self.BlowoutVars.PostHitActionTime = CT+1
+			end
+
+			if( self.BlowoutVars.PostHitActionTime < CT and self.BlowoutVars.BlowoutStep > 5 ) then
+				if( self.BlowoutVars.BlowoutStep < 7 ) then
+					self.BlowoutVars.BlowoutStep = 7
+					self.BlowoutVars.PostHitActionTime = CT + 6
+					print("waiting - phase 7")
+				elseif( self.BlowoutVars.BlowoutStep < 8 ) then
+					self.BlowoutVars.BlowoutStep = 8
+					self.BlowoutVars.PostHitActionTime = CT + 3.5
+					self:ChangePhase(4, 3.5)
+					print("clearing up - phase 8")
+				elseif( self.BlowoutVars.BlowoutStep < 9 ) then
+					self.BlowoutVars.BlowoutStep = 9
+					self.BlowoutVars.PostHitActionTime = CT + 8
+					self:ChangePhase(5,CurTime()+20)
+					print("finishing - phase 9")
+
+				elseif( self.BlowoutVars.BlowoutStep < 10 ) then
+					self.BlowoutVars.BlowoutStep = 10
+					self:PlaySoundOnClient( "posthit" )
+					self.BlowoutVars.PostHitActionTime = CT + 12
+					print("announce - phase 10")
+
+					self:ResetBlowoutVars()
+				elseif( self.BlowoutVars.BlowoutStep < 11 ) then
+					self:ChangePhase(0,0)
+					self:ResetBlowoutVars()
+
+					print("done - phase 11")
+				end
+			end
+
+			if self.BlowoutVars.NextHitSound < CT and self.BlowoutVars.BlowoutStep > 3 then		
+				self.BlowoutVars.NextHitSound = CT + math.Rand(0.75,4)
+				self:PlaySoundOnClient( "hit" )		--Play the thunder sounds
+
+				local e=ents.Create("env_shake")	--Shake the world a little
+				e:SetKeyValue( "amplitude", "3")
+				e:SetKeyValue("frequency","100.0")
+				e:SetKeyValue("duration","10")
+				e:SetKeyValue("spawnflags", "5")
+				e:Fire("startshake")
+			end
+
+			if self.BlowoutVars.NextAmbientSound == 0 then
+				self.BlowoutVars.NextAmbientSound = CT + 5
+			elseif CT > self.BlowoutVars.NextAmbientSound then
+				self.BlowoutVars.NextAmbientSound = CT + math.random(15,20)
+				self:PlaySoundOnClient( "ambient" )
+			end
+		end
+	end
+
+	function PLUGIN:PlaySoundOnClient(snd)
+		local ID = 0
+		if snd == "announce1" then ID = 1
+		elseif snd == "announce2" then ID = 2
+		elseif snd == "prehit" then ID = 3
+		elseif snd == "ambient" then ID = math.random(4,8)
+		elseif snd == "start" then ID = 9
+		elseif snd == "buildup" then ID = 10
+		elseif snd == "hit" then ID = math.random(11,13)
+		elseif snd == "end" then ID = 14
+		elseif snd == "posthit" then ID = 15
+		end
+
+		if ID > 0 then
+			net.Start("BlowoutPlaySound")
+				net.WriteUInt(ID, 8)
+			net.Broadcast()
+		end
+	end
+
+
+	function PLUGIN:ChangePhase(phase,duration)
+		net.Start("BlowoutChangePhase")
+			net.WriteUInt(phase, 8)
+			net.WriteFloat(duration or 0)
+		net.Broadcast()
+	end
+
+	function PLUGIN:IsPosSafe(pos)
+		local tracedata_up = {}
+		tracedata_up.start = pos
+		tracedata_up.endpos = pos + Vector(0,0,999999)
+		tracedata_up.mask = MASK_SOLID + MASK_WATER 
+		local trace_up = util.TraceLine(tracedata_up)	--Can the entity see the sky?
+    
+		if trace_up.HitSky then
+			local Pos_up = trace_up.HitPos
+			local tracedata_down = {}
+			tracedata_down.start = Pos_up
+			tracedata_down.endpos = pos
+			tracedata_down.mask = MASK_SOLID + MASK_WATER 
+			local trace_down = util.TraceLine(tracedata_down)	--Can the sky see the entity? (excludes entities that are below displacements or inside brushes)
+		
+			if (!trace_down.Hit) then
+				return false
+			end
+		
+			return true
+		end
+
+		return true
+	end
+
+	function PLUGIN:ResetBlowoutVars()
+		self.BlowoutVars.BlowoutStarted 	= false
+		self.BlowoutVars.PrehitTime  		= 0
+		self.BlowoutVars.BlastTime   		= 0
+		self.BlowoutVars.BuildupTime 		= 0
+		self.BlowoutVars.BlowoutHitTime   	= 0
+		self.BlowoutVars.FinalSoundTime  	= 0
+	
+		self.BlowoutVars.NextAmbientSound 	= 0
+		self.BlowoutVars.NextHitSound 		= 0
+		self.BlowoutVars.BlowoutStep 		= 0
+	
+		self.BlowoutVars.PostHitActionTime	= 0
+		self.BlowoutVars.BlowoutFinishTime	= 0
+
+		self.NextBlowout = CurTime() + 600
+	end
+end
+
+ix.command.Add("blowouttrigger", {
+	adminOnly = true,
+	OnRun = function(self, client)
+		PLUGIN.NextBlowout = CurTime()
+	end
+})
