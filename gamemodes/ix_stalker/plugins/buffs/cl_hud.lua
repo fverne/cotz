f5d1c83162c4e5c5fc27de9ca0fa1e2e9ad765f4
fagnig <fagnig@autistklassen.dk>
2020-12-12 14:25:54 +0100

    Add Icon for PsySupress buff
    Also adjusts width of icons

--- gamemodes/ix_stalker/plugins/buffs/cl_hud.lua
+++ gamemodes/ix_stalker/plugins/buffs/cl_hud.lua
@@ -7,36 +7,44 @@ function PLUGIN:HUDPaint()
 	local Texture3 = Material("vgui/hud/radmin.png", "noclamp smooth") 
 	local Texture4 = Material("vgui/hud/radpro.png", "noclamp smooth") 
 	local Texture5 = Material("vgui/hud/radmin.png", "noclamp smooth") 
+	local Texture6 = Material("vgui/hud/psypro.png", "noclamp smooth") 
 	
 
 	if LocalPlayer():HasBuff("buff_slowheal") then
 		surface.SetDrawColor(Color(255, 255, 255, 255))
 		surface.SetMaterial(Texture1)
-		surface.DrawTexturedRect(ScrW()*0.82, ScrH()*0.75, ScrW()*0.018, ScrH()*0.032)
+		surface.DrawTexturedRect(ScrW()*0.82, ScrH()*0.75, ScrW()*0.016, ScrH()*0.032)
 	end
 
 	if LocalPlayer():HasBuff("buff_staminarestore") then
 		surface.SetDrawColor(Color(255, 255, 255, 255))
 		surface.SetMaterial(Texture2)
-		surface.DrawTexturedRect(ScrW()*0.84, ScrH()*0.75, ScrW()*0.018, ScrH()*0.032)
+		surface.DrawTexturedRect(ScrW()*0.84, ScrH()*0.75, ScrW()*0.016, ScrH()*0.032)
 	end
 	
 	if LocalPlayer():HasBuff("buff_radiationremoval") then
 		surface.SetDrawColor(Color(255, 255, 255, 255))
 		surface.SetMaterial(Texture3)
-		surface.DrawTexturedRect(ScrW()*0.86, ScrH()*0.75, ScrW()*0.018, ScrH()*0.032)
+		surface.DrawTexturedRect(ScrW()*0.86, ScrH()*0.75, ScrW()*0.016, ScrH()*0.032)
 	end
 
 	if LocalPlayer():HasBuff("buff_radprotect") then
 		surface.SetDrawColor(Color(255, 255, 255, 255))
 		surface.SetMaterial(Texture4)
-		surface.DrawTexturedRect(ScrW()*0.88, ScrH()*0.75, ScrW()*0.018, ScrH()*0.032)
+		surface.DrawTexturedRect(ScrW()*0.88, ScrH()*0.75, ScrW()*0.016, ScrH()*0.032)
 	end
 
 	if LocalPlayer():HasBuff("debuff_radiation") then
 		surface.SetDrawColor(Color(255, 0, 0, 255))
 		surface.SetMaterial(Texture5)
-		surface.DrawTexturedRect(ScrW()*0.9, ScrH()*0.75, ScrW()*0.018, ScrH()*0.032)
+		surface.DrawTexturedRect(ScrW()*0.92, ScrH()*0.75, ScrW()*0.016, ScrH()*0.032)
 	end
+
+	if LocalPlayer():HasBuff("buff_psysuppress") then
+		surface.SetDrawColor(Color(255, 255, 255, 255))
+		surface.SetMaterial(Texture6)
+		surface.DrawTexturedRect(ScrW()*0.9, ScrH()*0.75, ScrW()*0.016, ScrH()*0.032)
+	end
+
 
 end
