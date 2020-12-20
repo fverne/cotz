ce92f50f7ddb7491b497cb5f37deb70b759c1a62
fagnig <fagnig@autistklassen.dk>
2020-12-20 12:21:38 +0100

    Fix quest system mistake

--- gamemodes/ix_stalker/plugins/questsystem/sh_definitions.lua
+++ gamemodes/ix_stalker/plugins/questsystem/sh_definitions.lua
@@ -1,4 +1,7 @@
 
+ix.quest = ix.quest or {}
+ix.quest.list = ix.quest.list or {}
+
 ix.quest.list["sidor_documents_special"] = { 		-- TEST QUEST
 	[10] = "Sidorovich wants to talk.", 			-- Gotten from PDA broadcast randomly
 	[20] = "Find the documents for Sidorovich", 	-- Gotten by talking to Sidorovich
