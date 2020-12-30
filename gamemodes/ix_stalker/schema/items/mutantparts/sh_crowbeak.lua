66deb7e28861f1edbd5212937bd010635da19287
fagnig <fagnig@autistklassen.dk>
2020-12-30 23:05:37 +0100

    mutant part rename

--- gamemodes/ix_stalker/schema/items/mutantparts/sh_crowbeak.lua
+++
@@ -1,8 +0,0 @@
-ITEM.name = "Crow Beak"
-ITEM.model ="models/lostsignalproject/items/parts/crow_beak.mdl"
-ITEM.description = "A beak from a crow."
-ITEM.longdesc = "While many stalkers consider crows no more than pests, more insightful ones acknowledge their usefulness in tackling the many challenges the Zone presents. While murders of crows can prove invaluable navigating and evading danger on the empty streets of Chernobyl and Pripyat, it's only after their death when their true value is manifested. The population of crows grows constantly in spite of the increased knowledge of usefulness of their beaks. It seems accelerated adaptation, typical among Zone's fauna, has provided them with a sped-up reproduction cycle resulting in up to 4 egg lays a year. Their prolonged life-span, of up to 20 years, has given them extraordinary intelligence manifested especially when many crows are in close proximity."
-
-ITEM.width = 1
-ITEM.height = 1
-ITEM.price = 50
