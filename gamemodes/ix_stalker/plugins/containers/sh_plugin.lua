4feed1471f982f252d06e3a02b1a7d9e42460249
Johnny <sellms@hotmail.com>
2021-01-05 15:40:30 +0100

    Exploit fix - Creds: SeamusMcGowan

--- gamemodes/ix_stalker/plugins/containers/sh_plugin.lua
+++ gamemodes/ix_stalker/plugins/containers/sh_plugin.lua
@@ -17,6 +17,11 @@ ix.config.Add("containerOpenTime", 0.7, "How long it takes to open a container."
 	category = "Containers"
 })
 
+ix.config.Add("containerPasswordDelay", 1, "How long a user has to wait between password attempts.", nil, {
+	data = {min = 0, max = 50},
+	category = "Containers"
+})
+
 function ix.container.Register(model, data)
 	ix.container.stored[model] = data
 end
@@ -172,12 +177,17 @@ if (SERVER) then
 		local password = net.ReadString()
 		local dist = entity:GetPos():DistToSqr(client:GetPos())
 
-		if (dist < 16384 and password) then
-			if (entity.password and entity.password == password) then
-				entity:OpenInventory(client)
-			else
-				client:NotifyLocalized("wrongPassword")
+		if (client.lastPasswordAttempt and CurTime() < client.lastPasswordAttempt + ix.config.Get("containerPasswordDelay",1)) then
+			client:NotifyLocalized("passwordTooQuick")
+		else
+			if (dist < 16384 and password) then
+				if (entity.password and entity.password == password) then
+					entity:OpenInventory(client)
+				else
+					client:NotifyLocalized("wrongPassword")
+				end
 			end
+			client.lastPasswordAttempt = CurTime()
 		end
 	end)
 
