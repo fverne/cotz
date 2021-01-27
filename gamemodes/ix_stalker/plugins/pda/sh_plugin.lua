1c6d8cd99578b8a21088e9971e2dffff19ce8365
fverne <frederikverne95@gmail.com>
2021-01-27 21:51:37 +0100

    npc pda talker added

--- gamemodes/ix_stalker/plugins/pda/sh_plugin.lua
+++ gamemodes/ix_stalker/plugins/pda/sh_plugin.lua
@@ -76,3 +76,37 @@ ix.char.RegisterVar("pdaavatar", {
 	default = "vgui/icons/face_31.png",
 	bNoDisplay = true,
 })
+
+-- NPC talker PDA
+local icon = Material("vgui/icons/news.png")
+ix.chat.Register("npcpdainternal", {
+	CanSay = function(self, speaker, text)
+	speaker:EmitSound( "stalkersound/pda/pda_news.wav", 55, 100, 0.2, CHAN_AUTO ) 
+
+		return true
+	end,
+	OnChatAdd = function(self, speaker, text, bAnonymous, data)
+		chat.AddText(Color(0,191,255), "[GPDA-"..data.name.."] ", Color(0,241,255), icon, ": "..data.message)
+	end,
+	prefix = {},
+	CanHear = function(self, speaker, listener)
+
+		return true
+	end,
+})
+
+ix.command.Add("npcpda", {
+	adminOnly = true,
+	description = "Write a PDA message from an arbitrary name",
+	arguments = {
+			ix.type.string,
+			ix.type.text
+		},
+	OnRun = function(self, client, npcname, message)
+		
+		ix.chat.Send(client, "npcpdainternal", message, nil, nil, {
+			name = npcname,
+			message = message
+		})
+	end
+})
