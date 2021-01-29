88c91378b8c217221550ff6754db735a05c69698
fverne <frederikverne95@gmail.com>
2021-01-29 22:28:25 +0100

    9vbattery valuable added

---
+++ gamemodes/ix_stalker/schema/items/valuable/sh_value_9vbattery.lua
@@ -0,0 +1,53 @@
+ITEM.name = "9 Volt Battery"
+ITEM.description = "Worn and dirty, but seems to be in working condition."
+ITEM.longdesc = ""
+ITEM.model = "models/lostsignalproject/items/misc/battery.mdl"
+
+ITEM.width = 1
+ITEM.height = 1
+ITEM.price = 0
+
+ITEM.quantity = 10
+
+ITEM.flatweight = 0
+ITEM.weight = 0.053
+
+function ITEM:GetPrice()
+	return self.price * self:GetData("quantity", self.quantity)
+end
+
+ITEM.functions.combine = {
+	OnCanRun = function(item, data)
+		if !data then
+			return false
+		end
+		local targetItem = ix.item.instances[data[1]]
+
+		if targetItem.uniqueid == item.uniqueid then
+			return true
+		else
+			return false
+		end
+	end,
+	OnRun = function(item, data)
+		local targetItem = ix.item.instances[data[1]]
+		local targetQuantDiff = targetItem.quantity - targetItem:GetData("quantity", targetItem.quantity)
+		local localQuant = item:GetData("quantity", item.quantity)
+		local targetQuant = targetItem:GetData("quantity", targetItem.quantity)
+		item.player:EmitSound("stalkersound/inv_properties.mp3", 110)
+		if targetQuantDiff >= localQuant then
+			targetItem:SetData("quantity", targetQuant + localQuant)
+			return true
+		else
+			item:SetData("quantity", localQuant - targetQuantDiff)
+			targetItem:SetData("quantity", targetItem.quantity)
+			return false
+		end
+	end,
+}
+
+function ITEM:OnInstanced()
+	if (!self:GetData("quantity")) then
+		self:SetData("quantity", math.random(2,3))
+	end
+end
