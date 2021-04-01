5b3ebeb85037ca2460adf3da7c8fe19b49949ec3
fagnig <fagnig@autistklassen.dk>
2021-04-01 20:24:03 +0200

    Fixed many small issues with AdvDupe2 integration

--- gamemodes/ix_stalker/plugins/advdupeint.lua
+++ gamemodes/ix_stalker/plugins/advdupeint.lua
@@ -17,7 +17,7 @@ ix.AdvDupeIntegration.CreatedEntities = {}
 -- dupetospawn: filename of the dupe to spawn
 -- uniqueid: special name to save this spawned dupe under to identify it again (eg. if multiple of the same dupe is spawned)
 -- offset: Vector offset from the original spawn position
-function PLUGIN:SpawnDupe(dupetospawn, uniqueid, offset)
+function PLUGIN:SpawnDupe(dupetospawn, uniqueid, offset, specificpos)
 	local path = AdvDupe2.DataFolder.."/STALKERDupes/"..dupetospawn..".txt"
 
 	if(not file.Exists(path, "DATA"))then print("File does not exist:", dupetospawn) return end
@@ -28,8 +28,9 @@ function PLUGIN:SpawnDupe(dupetospawn, uniqueid, offset)
 	local success, dupe, info, moreinfo = AdvDupe2.Decode(read)
 
 	if(success)then --This is where the magic happens
+		if (not uniqueid) then uniqueid = dupetospawn end
 
-		ix.AdvDupeIntegration.CreatedEntities[dupetospawn] = {}
+		ix.AdvDupeIntegration.CreatedEntities[uniqueid] = {}
 
 		ix.AdvDupeIntegration.Entities = {}
 		ix.AdvDupeIntegration.Constraints = {}
@@ -39,9 +40,8 @@ function PLUGIN:SpawnDupe(dupetospawn, uniqueid, offset)
 		ix.AdvDupeIntegration.Constraints = dupe["Constraints"]
 		ix.AdvDupeIntegration.HeadEnt = dupe["HeadEnt"]
 
-		local ents = AdvDupe2.duplicator.Paste( nil, ix.AdvDupeIntegration.Entities, ix.AdvDupeIntegration.Constraints, nil, nil, ix.AdvDupeIntegration.HeadEnt.Pos + (offset or Vector(0,0,0)), true )
+		local ents = AdvDupe2.duplicator.Paste( nil, ix.AdvDupeIntegration.Entities, ix.AdvDupeIntegration.Constraints, nil, nil, specificpos or ix.AdvDupeIntegration.HeadEnt.Pos + (offset or Vector(0,0,0)), true )
 
-		if (not uniqueid) then uniqueid = dupetospawn end
 		local PhysObj
 		for k,v in pairs(ents) do -- Enable motion
 			if(IsValid(v))then
@@ -66,6 +66,8 @@ function PLUGIN:SpawnDupe(dupetospawn, uniqueid, offset)
 end
 
 function PLUGIN:DespawnDupe(dupetodespawn)
+	if(!ix.AdvDupeIntegration.CreatedEntities[dupetodespawn]) then return end
+
 	for k,v in pairs(ix.AdvDupeIntegration.CreatedEntities[dupetodespawn]) do
 		if( IsValid(v) ) then v:Remove() end
 	end
@@ -102,4 +104,17 @@ end
 
 function ix.util.DepawnAdvDupe2Dupe( dupetodespawn )
 	ix.plugin.list["advdupeint"]:DespawnDupe(dupetodespawn)
+end
+
+function PLUGIN:PlayerSpawnSENT(client, class)
+	if (!client) then return true end -- When AdvDupe2 spawns
+	return client:IsAdmin()
+end
+
+function PLUGIN:PlayerSpawnedSENT(client, entity)
+	if (!client) then return true end -- When AdvDupe2 spawns
+end
+
+function PLUGIN:PlayerSpawnRagdoll(client)
+	if (!client) then return true end -- When AdvDupe2 spawns
 end
