376612ca5a46fe118bf2d30ba983b512bc12ecb0
frederik verne <fverne@baseret.dk>
2021-07-09 11:21:55 +0200

    changed home map

--- gamemodes/ix_cotz/plugins/simplecrossserver/sh_plugin.lua
+++ gamemodes/ix_cotz/plugins/simplecrossserver/sh_plugin.lua
@@ -6,7 +6,7 @@ PLUGIN.desc = "Allows for basic cross server interaction."
 
 PLUGIN.loadpoints = PLUGIN.loadpoints or {}
 PLUGIN.mapdata = PLUGIN.mapdata or {}
-PLUGIN.homemap = "rp_marsh_cs_cotz_v2"
+PLUGIN.homemap = "rp_marsh_cs"
 
 local DEBUGSETUP = true
 local checktime = 0
