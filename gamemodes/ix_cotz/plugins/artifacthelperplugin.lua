87caacc4dc86276ec30554bd8c95e5b938f3426c
Cyanide0999 <cyanidedzn@gmail.com>
2022-01-03 04:04:08 +0000

    Fix infinite high jump
    If you died with the high jump buff from the galaxy artifact, you would keep your jump power @ 460 instead of the default 160. This fixes it.

--- gamemodes/ix_cotz/plugins/artifacthelperplugin.lua
+++ gamemodes/ix_cotz/plugins/artifacthelperplugin.lua
@@ -2,16 +2,29 @@ PLUGIN.name = "Artifact Helper Plugin"
 PLUGIN.author = "gumlefar"
 PLUGIN.desc = "Helper functions and hooks for artifacts"
 
-function PLUGIN:EntityTakeDamage( target, dmginfo )
-	-- Capacitor
+function PLUGIN:EntityTakeDamage(target, dmginfo)
+    -- Capacitor
+    if (target:IsPlayer() and dmginfo:IsDamageType(DMG_SHOCK)) then
+        local cap = target:GetCharacter():GetInventory():HasItem("artifact_capacitor")
 
-	if ( target:IsPlayer() and dmginfo:IsDamageType(DMG_SHOCK)) then
-		local cap = target:GetCharacter():GetInventory():HasItem("artifact_capacitor")
-		if(cap and cap:GetData("cooldown",0) < os.time())then
-        	cap:SetData("cooldown", os.time() + 5 )
-        	dmginfo:SetDamage(0)
-
-        	ix.chat.Send(target, "iteminternal", "'s Capactior artifact absorbs the shock.", false)
+        if (cap and cap:GetData("cooldown", 0) < os.time()) then
+            cap:SetData("cooldown", os.time() + 5)
+            dmginfo:SetDamage(0)
+            ix.chat.Send(target, "iteminternal", "'s Capactior artifact absorbs the shock.", false)
         end
-	end
+    end
+end
+
+if SERVER then
+    function PLUGIN:DoPlayerDeath(client, attacker, dmg)
+        if not client:GetCharacter() then return end
+
+        if client:HasBuff("buff_hijump") then
+            client:RemoveBuff("buff_hijump", {
+                amount = 0
+            })
+
+            client:SetJumpPower(160)
+        end
+    end
 end
