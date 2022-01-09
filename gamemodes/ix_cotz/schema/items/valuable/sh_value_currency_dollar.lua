f2bccefc81da83b203743edf3aa6fb1f55e110a3
fverne <frederikverne95@gmail.com>
2022-01-09 17:41:58 +0100

    added dollars and euros

---
+++ gamemodes/ix_cotz/schema/items/valuable/sh_value_currency_dollar.lua
@@ -0,0 +1,60 @@
+ITEM.name = "Dollar Notes"
+ITEM.description = "A bundle of American currency."
+ITEM.longdesc = "Although hardly useful in hostile areas as the zone, cash such as the dollar notes are still of use to those who have connections to the outside of the zone, especially since the dollar is a strong currency worldwide."
+ITEM.model = "models/kek1ch/money_usa.mdl"
+
+ITEM.width = 1
+ITEM.height = 1
+ITEM.price = 38 -- lower than real conversion rate, intended
+
+ITEM.quantity = 5000000
+
+ITEM.flatweight = 0
+ITEM.weight = 0.010
+
+ITEM.exRender = true
+ITEM.iconCam = {
+	pos = Vector(0, 0, 200),
+	ang = Angle(90, 0, 0),
+	fov = 2.5,
+}
+
+function ITEM:GetPrice()
+	return self.price * self:GetData("quantity", self.quantity)
+end
+
+ITEM.functions.combine = {
+	OnCanRun = function(item, data)
+		if !data then
+			return false
+		end
+		local targetItem = ix.item.instances[data[1]]
+
+		if targetItem.uniqueID == item.uniqueID then
+			return true
+		else
+			return false
+		end
+	end,
+	OnRun = function(item, data)
+		local targetItem = ix.item.instances[data[1]]
+		local targetQuantDiff = targetItem.quantity - targetItem:GetData("quantity", targetItem.quantity)
+		local localQuant = item:GetData("quantity", item.quantity)
+		local targetQuant = targetItem:GetData("quantity", targetItem.quantity)
+		item.player:EmitSound("stalkersound/inv_properties.mp3", 110)
+		if targetQuantDiff >= localQuant then
+			targetItem:SetData("quantity", targetQuant + localQuant)
+			return true
+		else
+			item:SetData("quantity", localQuant - targetQuantDiff)
+			targetItem:SetData("quantity", targetItem.quantity)
+			return false
+		end
+	end,
+}
+
+function ITEM:OnInstanced()
+	if (!self:GetData("quantity")) then
+		self:SetData("quantity", 1)
+	end
+end
