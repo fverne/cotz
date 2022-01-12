cf3b03238e22a1771d004b8451fab537f8b6cbfd
fverne <frederikverne95@gmail.com>
2022-01-12 19:50:03 +0100

    cats now instakill (lore)

--- gamemodes/ix_cotz/plugins/huddisabler.lua
+++ gamemodes/ix_cotz/plugins/huddisabler.lua
@@ -1,7 +1,7 @@
 local PLUGIN = PLUGIN
 PLUGIN.name = "hidehud"
 PLUGIN.author = "verne"
-PLUGIN.desc = "hidet of hud"
+PLUGIN.desc = "hider of hud"
 
 ix.option.Add("disablehud", ix.type.bool, false, {
 	category = "_stalkersettings",
