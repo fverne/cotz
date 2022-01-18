1a654eb4b760c0c80469e7ef9db3fa73058d4b38
fverne <frederikverne95@gmail.com>
2022-01-18 16:34:19 +0100

    preliminary work for new buff icons

--- gamemodes/ix_cotz/schema/libs/sh_iconhelper.lua
+++ gamemodes/ix_cotz/schema/libs/sh_iconhelper.lua
@@ -1,7 +1,7 @@
 
 ix.util.text = ix.util.text or {}
 
-// returns material of the given item - either img, exicon, or the render
+-- returns material of the given item - either img or the exIcon
 function ix.util.GetItemIcon(item)
     local icon
     local exIcon = ikon:GetIcon(item.uniqueID)
@@ -23,13 +23,55 @@ function ix.util.GetItemIcon(item)
             local tmpIcon = ikon:GetIcon(item.uniqueID)
             item = tmpIcon
         end
-    else
-        -- We dont want to end up here...
-        -- USE img OR exRender PLEASE
-        icon = iconrow:Add("SpawnIcon")
-        icon:SetModel(item:GetModel())
-        icon:SetSize(48 * item.width, 48 * item.height)
     end
 
     return icon
+end
+
+
+-- draws status icons, such as hunger, thirst, radiation, with a coloured circle background
+function ix.util.DrawStatusIcon(iconstring, value, posX, posY, sizeX, sizeY)
+    local background = Material("placeholder/here.png")
+    local icon = Material(iconstring)
+
+    --draw the circle background, and color it accordingly
+    surface.SetMaterial(background)
+    if value > 85 then
+        surface.SetDrawColor(Color(0, 0, 0, 0))
+    elseif value > 75 then
+        surface.SetDrawColor(Color(131, 131, 131))
+    elseif value > 55 then
+        surface.SetDrawColor(Color(177, 255, 103))
+    elseif value > 35 then
+        surface.SetDrawColor(Color(255, 252, 91))
+    elseif value > 15 then
+        surface.SetDrawColor(Color(255, 183, 88))
+    elseif value <= 15 then
+        surface.SetDrawColor(Color(255, 106, 106))
+    end
+    surface.DrawTexturedRect(posX, posY, sizeX, sizeY)
+
+    --draw the icon on top of the background
+    if value <= 85 then
+        surface.SetMaterial(icon)
+        surface.SetDrawColor(Color(0, 0, 0, 255))
+        surface.DrawTexturedRect(posX, posY, sizeX, sizeY) -- TODO: Make this smaller than the background
+    end
+end
+
+
+-- draws buff icons, such as healing, psyblock, staminaregen with a white square background
+function ix.util.DrawBuffIcon(iconstring, posX, posY, sizeX, sizeY)
+    local background = Material("placeholder/here.png")
+    local icon = Material(iconstring)
+
+    --draw the square background, and color it accordingly
+    surface.SetMaterial(background)
+    surface.SetDrawColor(Color(255, 255, 255))
+    surface.DrawTexturedRect(posX, posY, sizeX, sizeY)
+
+    --draw the icon on top of the background
+    surface.SetMaterial(icon)
+    surface.SetDrawColor(Color(0, 0, 0, 255))
+    surface.DrawTexturedRect(posX, posY, sizeX, sizeY) -- TODO: Make this smaller than the background
 end
