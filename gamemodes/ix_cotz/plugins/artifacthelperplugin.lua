b552e115696675bd5392b998879cc049b51f36d4
Cyanide0999 <cyanidedzn@gmail.com>
2022-01-28 19:24:41 +0000

    jumping artifacts fix

--- gamemodes/ix_cotz/plugins/artifacthelperplugin.lua
+++ gamemodes/ix_cotz/plugins/artifacthelperplugin.lua
@@ -33,23 +33,30 @@ local thinkTime = CurTime()
 
 function PLUGIN:Think()
     if (thinkTime < CurTime()) then
-        for k, v in ipairs(player.GetAll()) do
+        for k, v in pairs(ents.FindInSphere(v2:GetPos(), 2560)) do
             for k2, v2 in ipairs(ents.FindByClass("ix_item")) do
                 if (IsValid(v2) and v2.ixItemID) then
                     local itm = ix.item.instances[v2.ixItemID]
 
-                    if (itm and itm.base == "base_artifacts" and !itm.NoJump) then
-                        if v:GetPos():Distance(v2:GetPos()) < 192 then
-                            v2:GetPhysicsObject():EnableMotion(true)
-                            v2:GetPhysicsObject():ApplyForceCenter(Vector(0, 0, itm.JumpPower or v2:GetPhysicsObject():GetMass() * 90))
-                        else
-                            v2:GetPhysicsObject():EnableMotion(false)
+                    if (itm and itm.base == "base_artifacts" and not itm.NoJump) then
+                        for k, v in pairs(ents.FindInSphere(v2:GetPos(), 2560)) do
+                            if (v:IsPlayer() and v:GetCharacter() and v:GetMoveType() ~= MOVETYPE_NOCLIP) then
+                                if v:GetPos():Distance(v2:GetPos()) <= 192 then
+                                    v2:GetPhysicsObject():EnableMotion(true)
+                                    v2:GetPhysicsObject():ApplyForceCenter(Vector(0, 0, itm.JumpPower or v2:GetPhysicsObject():GetMass() * 90))
+                                    break
+                                end
+                            else
+                                if v:GetPos():Distance(v2:GetPos()) > 192 then
+                                    v2:GetPhysicsObject():EnableMotion(false)
+                                end
+                            end
                         end
                     end
                 end
-            end
 
-            thinkTime = CurTime() + math.Rand(0.5, 0.8)
+                thinkTime = CurTime() + math.Rand(0.5, 0.8)
+            end
         end
     end
 end
