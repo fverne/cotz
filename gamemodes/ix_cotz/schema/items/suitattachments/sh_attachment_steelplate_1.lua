ca82e025695cf27f096d44b33c8166e68ce32600
Frederik Verne Henriksen <frederikverne95@gmail.com>
2022-02-13 17:31:36 +0100

    progression item stats update

--- gamemodes/ix_cotz/schema/items/suitattachments/sh_attachment_steelplate_1.lua
+++ gamemodes/ix_cotz/schema/items/suitattachments/sh_attachment_steelplate_1.lua
@@ -5,7 +5,7 @@ ITEM.category = "Attachments"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.upgradeIcon = Material("materials/vgui/ui/stalker/weaponupgrades/grip.png")
-ITEM.price = 10000
+ITEM.price = 7500
 
 ITEM.attachName = "steelplate_1"
 
