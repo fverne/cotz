cd09aab2eba4c6703774a6e592f20294ff674f9c
fagnig <fagnig@autistklassen.dk>
2022-02-13 23:08:48 +0100

    Add technician tool items

---
+++ gamemodes/ix_cotz/schema/items/valuable/sh_value_techtool_1.lua
@@ -0,0 +1,17 @@
+ITEM.name = "Box of Basic Tools"
+ITEM.description = "A box of assorted tools."
+ITEM.longdesc = "A collection of common tools for technical work. Most of them are in bad condition, but a technician might find some use for them."
+ITEM.model = "models/lostsignalproject/items/quest/toolkit_1.mdl"
+
+ITEM.width = 2
+ITEM.height = 1
+ITEM.price = 4000
+
+ITEM.flatweight = 2.000
+
+ITEM.exRender = true
+ITEM.iconCam = {
+	pos = Vector(0, 0, 200),
+	ang = Angle(90, 180, 0),
+	fov = 5.5,
+}
---
+++ gamemodes/ix_cotz/schema/items/valuable/sh_value_techtool_2.lua
@@ -0,0 +1,18 @@
+ITEM.name = "Box of Advanced Tools"
+ITEM.description = "A box of assorted tools."
+ITEM.longdesc = "A collection of tools suited for fine technical work. A few of them are broken."
+ITEM.model = "models/lostsignalproject/items/quest/toolkit_2.mdl"
+
+ITEM.width = 2
+ITEM.height = 1
+ITEM.price = 8000
+
+ITEM.flatweight = 4.000
+
+
+ITEM.exRender = true
+ITEM.iconCam = {
+	pos = Vector(0, 0, 200),
+	ang = Angle(90, 180, 0),
+	fov = 5.5,
+}
