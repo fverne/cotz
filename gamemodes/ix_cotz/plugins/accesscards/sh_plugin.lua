71f636462704cdf132c7424a4e8a35fffc65d294
Frederik Verne Henriksen <frederikverne95@gmail.com>
2022-03-23 21:51:21 +0100

    access cards (ecologist) added :)

---
+++ gamemodes/ix_cotz/plugins/accesscards/sh_plugin.lua
@@ -0,0 +1,5 @@
+local PLUGIN = PLUGIN
+PLUGIN.name = "Access Cards"
+PLUGIN.author = "verne"
+PLUGIN.desc = "Access cards for doors and func_doors."
+
--- gamemodes/ix_cotz/plugins/mapedits.lua
+++ gamemodes/ix_cotz/plugins/mapedits.lua
@@ -3,9 +3,8 @@ PLUGIN.author = "verne"
 PLUGIN.description = "mapedits - primarily for rp_salvation_haven"
 
 function PLUGIN:EntityKeyValue(ent, key, value)
-    if IsValid(ent) and ent:GetClass() == "func_button" and ent:GetName() == "button_down" then
-        
-        if value != 5 then
+    if game.GetMap() == "rp_salvation_haven" then
+        if IsValid(ent) and ent:GetClass() == "func_button" and ent:GetName() == "button_down" then
             ent:SetKeyValue("wait", 5)
         end
     end
