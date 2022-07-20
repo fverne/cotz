d3925fe02d5607f680200b3d52ed5c43c5c25fe6
Cyanide0999 <cyanidedzn@gmail.com>
2022-07-20 17:30:34 +0100

    final voice icons fix

--- gamemodes/ix_cotz/plugins/voicerevamped/sv_plugin.lua
+++ gamemodes/ix_cotz/plugins/voicerevamped/sv_plugin.lua
@@ -4,10 +4,8 @@ function PLUGIN:InitializedPlugins()
     RunConsoleCommand("mp_show_voice_icons", "0")
 end
 
-function PLUGIN:PlayerLoadedCharacter(client, character)
-    timer.Simple(0.25, function()
-        client:SetLocalVar("voiceRange", 2)
-    end)
+function PLUGIN:PlayerSpawn(client)
+    client:SetNetVar("voiceRange", 2)
 end
 
 /*
