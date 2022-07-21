b8a779649dbaba17ebf2e60ea9a4cd09be299b98
Cyanide0999 <cyanidedzn@gmail.com>
2022-07-21 18:55:06 +0100

    pda map config

--- !unused plugins/orthographic_map/sh_plugin.lua
+++ !unused plugins/orthographic_map/sh_plugin.lua
@@ -5,10 +5,16 @@ PLUGIN.description = "A orthographic map for the PDA that is based off the games
 ix.util.Include("derma/cl_pdamap.lua")
 ix.util.Include("derma/cl_pdamarkeredit.lua")
 
+ix.config.Add("PDAOrthographicMap", true, "If true, the pda map will appear in the encylopedia tab.", nil, {
+    category = "1development"
+})
+
 function PLUGIN:PopulateHelpMenu(tabs)
-    tabs["map"] = function(container)
-        local mapFrame = vgui.Create("ixPDAMap")
-        ix.gui.lastHelpMenuTab = "readme"
-        ix.gui.menu:Hide()
+    if ix.config.Get("PDAOrthographicMap", true) then
+        tabs["map"] = function(container)
+            local mapFrame = vgui.Create("ixPDAMap")
+            ix.gui.lastHelpMenuTab = "readme"
+            ix.gui.menu:Hide()
+        end
     end
 end
