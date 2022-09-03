3c79723984b4e71986a26766f9ace0c82a2613ca
fagnig <fagnig@autistklassen.dk>
2022-09-03 23:41:44 +0200

    Add almost all of main progression line

--- gamemodes/ix_cotz/plugins/progression/sh_definitions.lua
+++ gamemodes/ix_cotz/plugins/progression/sh_definitions.lua
@@ -54,6 +54,26 @@ hook.Add("ix_OnJobComplete", "CookNPC_cookMeatCollect", function(client, npciden
 	end
 end)
 
+hook.Add("ix_OnJobComplete", "Mute_scanTasks", function(client, npcidentifier, identifier)
+	local iscorrecttasktype = false
+
+	local killcategories = {
+		["scanareaeasy"] = true,
+		["scanareamed"] = true,
+		["scanareahigh"] = true,
+	}
+
+	for k, v in pairs(ix.jobs.list[identifier].categories) do
+		if killcategories[v] then iscorrecttasktype = true end
+	end
+
+	if npcidentifier == "'Mute'" and iscorrecttasktype then
+		if ix.progression.IsActive("stalkerNetAdmin_AreaTasks") then
+			ix.progression.AddProgessionValue("stalkerNetAdmin_AreaTasks", 1, client:Name())
+		end
+	end
+end)
+
 ix.progression.Register("oldTimerKillIntro", {
 	name = "Cleaning up the Zone",
 	description = "Cleaning up the zone",
@@ -140,12 +160,12 @@ ix.progression.Register("oldTimerKillIntro", {
 		},
 	},
 	progressthresholds = {
-		[1] = 1,
-		[2] = 1, -- just for the dupe spawning
-		[3] = 2,
-		[4] = 2,
-		[5] = 3,
-		[6] = 3
+		[1] = 30,
+		[2] = 30, -- just for the dupe spawning
+		[3] = 55,
+		[4] = 55,
+		[5] = 110,
+		[6] = 110
 	}
 })
 
@@ -540,7 +560,17 @@ ix.progression.Register("stalkerNetAdminDelivery_mainRadioTower", {
 			end)
 
 			-- Add quest item to vendor list that lets players activate next progression step at quartermaster on map2
+			local npc = ix.progression.GetNPCFromName("'Mute'")
+			if (npc) then
+				npc:AddItemToList("quest_flashdrive", nil, 1, "SELLANDBUY", 1, 3, 1) -- Main Progression
+			end
 
+			timer.Simple(90, function()
+				ix.chat.Send(nil, "npcpdainternal", "", nil, nil, {
+					name = "'Haggler'",
+					message = "Hello everyone! I see you're all doing quite well for yourselves. I have some wares if you got the cash."
+				})
+			end)
 			-- Needs correct pos/ang set
 			--local pos = Vector(-6126.198730, -9746.725586, 4959.031250)
 			--local ang = Angle(0.000, -178.989, 0.000)
@@ -548,7 +578,186 @@ ix.progression.Register("stalkerNetAdminDelivery_mainRadioTower", {
 
 			ix.progression.SetActive("stalkerNetAdminDelivery_mainRadioTower", false)
 
+			
+		end
+	end
+})
+
+ix.progression.Register("quarterMasterDelivery_activateItem", {
+	name = "Making Connections",
+	description = "Bring the flash drive to Quartermaster.",
+	keyNpc = "'Quartermaster'",
+	defaultActive = false,
+	BuildResponse = function(self, status)
+		return "I chatted with a guy, what was his name, Mute? He should have a flash drive with information for me. I ain't budging 'till I get that.\n"
+	end,
+	GetItemIds = function()
+		local itemids = {
+			["quest_flashdrive"] 	= 1,
+		}
+
+		return itemids
+	end,
+	progressfunctions = {
+		[1] = {
+			OnRun = function()
+				
+			end,
+			RunOnce = false,
+		},
+	},
+	progressthresholds = {
+		[1] = 1,
+	},
+	fnAddComplexProgression = function(dat, playername)
+		ix.progression.status["quarterMasterDelivery_activateItem"].complexData = ix.progression.status["quarterMasterDelivery_activateItem"].complexData or {}
+		ix.progression.status["quarterMasterDelivery_activateItem"].complexData[dat] = ix.progression.status["quarterMasterDelivery_activateItem"].complexData[dat] or 0
+		ix.progression.status["quarterMasterDelivery_activateItem"].complexData[dat] = ix.progression.status["quarterMasterDelivery_activateItem"].complexData[dat]+1
+	end,
+	fnGetComplexProgression = function()
+		return ix.progression.status["quarterMasterDelivery_activateItem"].complexData
+	end,
+	fnCheckComplexProgression = function()
+		local finished =  ix.progression.definitions["quarterMasterDelivery_activateItem"]:GetItemIds()
+
+		local isdone = true
+
+		for item, amt in pairs(ix.progression.status["quarterMasterDelivery_activateItem"].complexData) do
+			if amt < finished[item] then isdone = false end
+		end
+
+		if isdone then
+			
+			timer.Simple(5, function()
+				ix.chat.Send(nil, "npcpdainternal", "", nil, nil, {
+					name = "'Quartermaster'",
+					message = "Alright, we can talk business, come over here."
+				})
+			end)
+
+			ix.progression.SetActive("quarterMasterDelivery_activateItem", false)
+
 			ix.progression.SetActive("quarterMasterDelivery_main", true) -- Main Progression
 		end
 	end
+})
+
+ix.progression.Register("quarterMasterDelivery_main", {
+	name = "Whetting the Whistle",
+	description = "Bring Quartermaster something to drink.",
+	keyNpc = "'Quartermaster'",
+	defaultActive = false,
+	BuildResponse = function(self, status)
+		ix.progression.status["quarterMasterDelivery_main"] = ix.progression.status["quarterMasterDelivery_main"] or {}
+		local dat = ix.progression.status["quarterMasterDelivery_main"].complexData
+		local itemids = self:GetItemIds()
+
+		local str = "Alright boys, lets see some hands, who likes booze? That's right, I do. I'm gonna need you to find some stuff to drink, and smoke. I made a little list for you so you wont forget:\n"
+
+		for item, amt in pairs(itemids) do
+			local tmp = 0
+			if (dat and dat[item]) then tmp = dat[item] end
+			str = str..string.format("\n%d %s", amt - tmp, ix.item.list[item].name)
+		end
+
+		str = str.."\n\nNow chop-chop, get going, that booze isn't gonna find itself."
+
+		return str
+	end,
+	GetItemIds = function()
+		local itemids = {
+			["drink_spirits_3"] 	= 10,
+			["drink_vodka_5"] 		= 10,
+			["drink_vodka_6"] 		= 10,
+			["drug_cigar"] 			= 40,
+			["drug_cocaine"] 		= 5,
+			["drug_cigarette_5"] 	= 30,
+			["drug_cigarette_6"] 	= 30,
+		}
+
+		return itemids
+	end,
+	progressfunctions = {
+		[1] = {
+			OnRun = function()
+				
+			end,
+			RunOnce = false,
+		},
+	},
+	progressthresholds = {
+		[1] = 1,
+	},
+	fnAddComplexProgression = function(dat, playername)
+		ix.progression.status["quarterMasterDelivery_main"].complexData = ix.progression.status["quarterMasterDelivery_main"].complexData or {}
+		ix.progression.status["quarterMasterDelivery_main"].complexData[dat] = ix.progression.status["quarterMasterDelivery_main"].complexData[dat] or 0
+		ix.progression.status["quarterMasterDelivery_main"].complexData[dat] = ix.progression.status["quarterMasterDelivery_main"].complexData[dat]+1
+	end,
+	fnGetComplexProgression = function()
+		return ix.progression.status["quarterMasterDelivery_main"].complexData
+	end,
+	fnCheckComplexProgression = function()
+		local finished =  ix.progression.definitions["quarterMasterDelivery_main"]:GetItemIds()
+
+		local isdone = true
+
+		for item, amt in pairs(ix.progression.status["quarterMasterDelivery_main"].complexData) do
+			if amt < finished[item] then isdone = false end
+		end
+
+		if isdone then
+			
+			timer.Simple(5, function()
+				ix.chat.Send(nil, "npcpdainternal", "", nil, nil, {
+					name = "'Quartermaster'",
+					message = "Thanks for the booze, I got keycards for the bunker for sale, if you got the cash for it."
+				})
+			end)
+
+			local npc = ix.progression.GetNPCFromName("'Quartermaster'")
+			if (npc) then
+				npc:AddItemToList("accesscard_ecologists", nil, 5, "SELLANDBUY", 5, 3, 5) -- Main Progression
+			end
+
+			ix.progression.SetActive("quarterMasterDelivery_main", false)
+
+			ix.progression.SetActive("egghead_dataTasks", true) -- Main Progression
+		end
+	end
+})
+
+ix.progression.Register("egghead_dataTasks", {
+	name = "Extracting Information",
+	description = "Scanning the Zone",
+	keyNpc = "'Egghead'",
+	defaultActive = true,
+	BuildResponse = function(self, status)
+		-- Find next treshold
+		local tresh = 0
+
+		for k,v in ipairs( self.progressthresholds ) do
+			if v > status.value then
+				tresh = v
+				break
+			end
+		end
+
+		return string.format("You'll need to complete %d contracts relating to extracting data from computers in the old underground complexes around the zone.", tresh-status.value)
+	end,
+	progressfunctions = {
+		[1] = {
+			OnRun = function()			
+				ix.chat.Send(nil, "npcpdainternal", "", nil, nil, {
+					name = "'Egghead'",
+					message = "Good work stalkers, you've gotten me a lot of interesting information, I think I might have an idea what has happened here, please come to the bunker for further jobs."
+				})
+
+				-- Add computer activation item to vendorlist here
+			end,
+			RunOnce = true
+		},
+	},
+	progressthresholds = {
+		[1] = 100,
+	}
 })
---
+++ gamemodes/ix_cotz/schema/items/miscellaneous/sh_quest_flashdrive.lua
@@ -0,0 +1,16 @@
+ITEM.name = "Flash Drive"
+ITEM.description = "A small plastic flash drive containing encrypted information."
+ITEM.longdesc = "A memory stick containing sensitive encrypted data for Quartermaster."
+ITEM.model = "models/lostsignalproject/items/quest/usb2.mdl"
+
+ITEM.width = 1
+ITEM.height = 1
+
+ITEM.price = 4200
+
+ITEM.exRender = true
+ITEM.iconCam = {
+	pos = Vector(0.40000000596046, 0, 50),
+	ang = Angle(90, 45, 0),
+	fov = 5,
+}
