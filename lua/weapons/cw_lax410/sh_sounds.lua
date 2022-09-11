c7378e0d2442d8a10949b878f580a96066b81c78
fagnig <fagnig@autistklassen.dk>
2022-09-11 16:51:46 +0200

    Add LAX410

---
+++ lua/weapons/cw_lax410/sh_sounds.lua
@@ -0,0 +1,5 @@
+CustomizableWeaponry:addFireSound("LAX410_FIRE", {"weapons/lax/wz_fire.wav","weapons/lax/bocw_fire.wav"}, 1, 125, CHAN_STATIC)
+
+CustomizableWeaponry:addReloadSound("LAX410_OPEN", "weapons/lax/open.wav")
+CustomizableWeaponry:addReloadSound("LAX410_CLOSE", "weapons/lax/close.wav")
+CustomizableWeaponry:addReloadSound("LAX410_INSERT", "weapons/lax/shell.wav")
