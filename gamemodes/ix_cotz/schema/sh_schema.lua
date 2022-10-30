9528b59572c05f1e4006addc03698369e0b282b2
Frederik Verne Henriksen <frederikverne95@gmail.com>
2022-10-30 16:36:37 +0100

    seva no longer tposes

--- gamemodes/ix_cotz/schema/sh_schema.lua
+++ gamemodes/ix_cotz/schema/sh_schema.lua
@@ -65,6 +65,7 @@ local stalker_models = {
 	["models/nasca/stalker/male_jupiter_duty.mdl"]= true,
 	["models/nasca/stalker/male_jupiter_eco.mdl"]= true,
 	["models/nasca/stalker/male_nbc_lone.mdl"] = true,
+	["models/nasca/stalker/male_seva_lone.mdl"] = true,
 }
 
 for k, v in pairs(stalker_models) do
