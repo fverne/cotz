27da6abb85ec0e850827645aa83d8bba60bada5d
Frederik Verne Henriksen <frederikverne95@gmail.com>
2022-12-16 06:44:26 +0100

    made headers key value

--- gamemodes/ix_cotz/plugins/discordrelay/libs/sh_discordrelay.lua
+++ gamemodes/ix_cotz/plugins/discordrelay/libs/sh_discordrelay.lua
@@ -1,11 +1,6 @@
 ix.util = ix.util or {}
 
--- local function EncodePdaAvatar(path)
---     local avatarFile = file.Read(path)
---     local base64avatar = util.Base64Encode(avatarfile)
 
---     return base64avatar
--- end
 
 function ix.util.DoHttpPost(name, text)
     local name = name or "Unknown"
@@ -19,8 +14,8 @@ function ix.util.DoHttpPost(name, text)
 			charname = name
 		},
 		headers = {
-			Content-Type = "application/json",
-			accept = "*/*"
+			['Content-Type'] = 'application/json',
+			['accept'] = '*/*'
 		},
 		success = function (code, body, headers) end,
 		failed = function (reason) Msg(reason) end
