b5afd5cd9825258bf374822aa07c1f62eb54aa7a
Cyanide0999 <cyanidedzn@gmail.com>
2023-01-14 15:10:04 +0000

    tentative fix item related exploit
    needs proper testing, should prevent item interactions in containers which is the cause for various exploits at the moment.

--- gamemodes/helix/gamemode/core/libs/sh_item.lua
+++ gamemodes/helix/gamemode/core/libs/sh_item.lua
@@ -583,6 +583,10 @@ do
 				return
 			end
 
+			if invID != client:GetCharacter():GetInventory:GetID() then
+				return
+			end
+			
 			local inventory = ix.item.inventories[invID or 0]
 
 			if (hook.Run("CanPlayerInteractItem", client, action, item, data) == false) then
