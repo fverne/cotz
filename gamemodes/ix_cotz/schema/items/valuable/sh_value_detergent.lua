74f7fb2531fa883f41a9ff0d0a26dbc2092e4491
Xenokkah <56182717+Xenokkah@users.noreply.github.com>
2023-01-23 21:02:17 -0500

    Update sh_value_detergent.lua
    Added longdesc to alkaline booster

--- gamemodes/ix_cotz/schema/items/valuable/sh_value_detergent.lua
+++ gamemodes/ix_cotz/schema/items/valuable/sh_value_detergent.lua
@@ -1,6 +1,6 @@
 ITEM.name = "Alkaline Booster"
 ITEM.description = "A plastic can of chemicals used in a washing machine."
-ITEM.longdesc = ""
+ITEM.longdesc = "Alkaline boosters are industrial chemicals used to boost the pH levels of whatever they're mixed into."
 ITEM.model = "models/illusion/eftcontainers/alkali.mdl"
 
 ITEM.width = 1
