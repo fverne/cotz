2bf1a1ab7e2c595056d6b4529f53691c25a0c10e
fagnig <fagnig@autistklassen.dk>
2023-06-16 00:50:11 +0200

    Add two new weapons

---
+++ lua/weapons/cw_mp18/sh_sounds.lua
@@ -0,0 +1,7 @@
+CustomizableWeaponry:addFireSound("CW_MP18_FIRE", "weapons/darsu_eft/mp18/mr18_fire_close.ogg", 1, 125, CHAN_STATIC)
+
+CustomizableWeaponry:addReloadSound("CW_MP18_LOCK", 	"weapons/darsu_eft/mp18/mr18_barrel_lock.ogg")
+CustomizableWeaponry:addReloadSound("CW_MP18_OPEN", 	"weapons/darsu_eft/mp18/mr18_barrel_open.ogg")
+CustomizableWeaponry:addReloadSound("CW_MP18_OUT", 		"weapons/darsu_eft/mp18/mr18_round_out1.ogg")
+CustomizableWeaponry:addReloadSound("CW_MP18_IN", 		"weapons/darsu_eft/mp18/mr18_round_in1.ogg")
+CustomizableWeaponry:addReloadSound("CW_MP18_CLOSE", 	"weapons/darsu_eft/mp18/mr18_barrel_close.ogg")
---
+++ lua/weapons/cw_toz106/sh_sounds.lua
@@ -0,0 +1,7 @@
+CustomizableWeaponry:addFireSound("CW_TOZ106_FIRE", "weapons/darsu_eft/toz106/toz_fire_close1.ogg", 1, 115, CHAN_STATIC)
+
+CustomizableWeaponry:addReloadSound("CW_TOZ106_BOLTOUT", "weapons/darsu_eft/toz106/toz_boltout.ogg")
+CustomizableWeaponry:addReloadSound("CW_TOZ106_BOLTIN", "weapons/darsu_eft/toz106/toz_boltin.ogg")
+
+CustomizableWeaponry:addReloadSound("CW_TOZ106_MAGOUT", "weapons/darsu_eft/toz106/sv98_mag_out.ogg")
+CustomizableWeaponry:addReloadSound("CW_TOZ106_MAGIN", "weapons/darsu_eft/toz106/sv98_mag_in.ogg")
