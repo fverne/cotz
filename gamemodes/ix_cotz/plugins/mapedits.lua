7257bd8448a7348c142fe306bb1e6823285d7f0d
Frederik Verne Henriksen <frederikverne95@gmail.com>
2023-07-21 21:46:54 +0200

    fixed mapedits for haven

--- gamemodes/ix_cotz/plugins/mapedits.lua
+++ gamemodes/ix_cotz/plugins/mapedits.lua
@@ -1,9 +1,9 @@
 PLUGIN.name = "mapedits"
 PLUGIN.author = "verne"
-PLUGIN.description = "mapedits - primarily for rp_salvation_haven"
+PLUGIN.description = "mapedits - primarily for rp_salvation_2_haven"
 
 function PLUGIN:EntityKeyValue(ent, key, value)
-    if game.GetMap() == "rp_salvation_haven" then
+    if game.GetMap() == "rp_salvation_2_haven" then
         -- fixes the elevator button not resetting after being pressed once
         if IsValid(ent) and ent:GetClass() == "func_button" and ent:GetName() == "button_down" then
             ent:SetKeyValue("wait", 5)
