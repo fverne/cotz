c9047feb8beffecf18bf847ae0871556a2c3d7c9
Frederik Verne Henriksen <frederikverne95@gmail.com>
2023-07-24 03:03:05 +0200

    added hellhound tail

---
+++ gamemodes/ix_cotz/schema/items/mutantparts/sh_part_hellhound.lua
@@ -0,0 +1,19 @@
+ITEM.name = "Hellhound Tail"
+ITEM.model ="models/lostsignalproject/items/parts/dog_tail.mdl"
+ITEM.material = "models/effects/splode_sheet"
+ITEM.description = "A fiery dog tail. Hot to the touch."
+ITEM.longdesc = "Unlike the regular dogs tail, the hellhound tail seems to be the only noticable difference based on various autopsies on both blind dogs, and hellhounds. Research seems to suggest that the fiery breath is actually being shot through the body, originating from the tail itself. How this exactly works is a mystery to this day."
+
+ITEM.width = 1
+ITEM.height = 1
+ITEM.price = 3350
+ITEM.pricepertier = 450
+ITEM.baseweight = 0.500
+ITEM.varweight = 0.100
+
+ITEM.exRender = true 
+ITEM.iconCam = {
+	pos = Vector(-1, 0, 200),
+	ang = Angle(90, 0, 0),
+	fov = 4.1176470588235,
+}
