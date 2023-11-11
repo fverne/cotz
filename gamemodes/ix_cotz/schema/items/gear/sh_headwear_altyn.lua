306321365323f7a72d32c2e71c8b607a9ffd24ea
fverne <frederikverne95@gmail.com>
2023-11-11 20:31:53 +0100

    made altyn headwear instead of helm

--- gamemodes/ix_cotz/schema/items/gear/sh_headwear_altyn.lua
+++ gamemodes/ix_cotz/schema/items/gear/sh_headwear_altyn.lua
@@ -1,12 +1,12 @@
 ITEM.name = "Altyn Helmet"
-ITEM.description = "A helmet."
+ITEM.description = "A steel helmet."
 ITEM.longdesc = "TODO"
 ITEM.model = "models/shtokerbox/ground_headgear_atlyn.mdl"
 
 ITEM.price = 30000
 ITEM.weight = 1
 
-ITEM.isGasmask = false
+ITEM.isGasmask = true
 ITEM.isHelmet = true
 ITEM.ballisticlevels = {"ll-a"}
 
