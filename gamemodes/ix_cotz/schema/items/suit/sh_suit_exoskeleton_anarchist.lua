93b6f34170aa95b5f1f63c50b48ee60f65a98bfa
fverne <frederikverne95@gmail.com>
2023-12-30 16:24:12 +0100

    added exoskeleton variants

---
+++ gamemodes/ix_cotz/schema/items/suit/sh_suit_exoskeleton_anarchist.lua
@@ -0,0 +1,37 @@
+ITEM.name = "Defender of Anarchists"
+ITEM.model = "models/kek1ch/exolight_outfit.mdl"
+ITEM.newModel = "models/stalkerisaac/playermodel/male_01_radsuit.mdl"
+ITEM.description= "An armored exoskeleton suit"
+ITEM.longdesc = "TODO"
+
+ITEM.width = 2
+ITEM.height = 3
+ITEM.img = ix.util.GetMaterial("vgui/hud/radsuit.png")
+ITEM.weight = 32.50
+ITEM.newSkin = 14
+
+ITEM.price = 1810200
+ITEM.repairCost = ITEM.price/100*1 -- cost to repair from 0% durability
+
+ITEM.isGasmask = false
+ITEM.isHelmet = false
+
+ITEM.br = 0.45
+ITEM.fbr = 5
+ITEM.sr = 0.81
+ITEM.fsr = 5
+ITEM.ar = 0.27
+ITEM.far = 3
+
+ITEM.carryinc = 64.000
+ITEM.miscslots = 3
+
+
+ITEM.skincustom[1] = {
+	name = "Skin 0",
+	skingroup = 0,
+}
+ITEM.skincustom[2] = {
+	name = "Skin 1",
+	skingroup = 1,
+}
