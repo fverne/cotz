67fbc4613030ab7c8c8b067c8759a5216e9e8626
fverne <frederikverne95@gmail.com>
2023-12-31 11:43:41 +0100

    more skin work

--- gamemodes/ix_cotz/schema/items/suit/sh_suit_berill_authority.lua
+++ gamemodes/ix_cotz/schema/items/suit/sh_suit_berill_authority.lua
@@ -27,11 +27,55 @@ ITEM.carryinc = 11.000
 ITEM.miscslots = 1
 
 
-ITEM.skincustom[1] = {
-	name = "Skin 0",
-	skingroup = 0,
+-- ITEM.skincustom[1] = {
+-- 	name = "Skin 0",
+-- 	skingroup = 0,
+-- }
+-- ITEM.skincustom[2] = {
+-- 	name = "Skin 1",
+-- 	skingroup = 1,
+-- }
+-- ITEM.skincustom[3] = {
+-- 	name = "Skin 2",
+-- 	skingroup = 2,
+-- }
+ITEM.skincustom[4] = {
+	name = "Skin 3",
+	skingroup = 3,
 }
-ITEM.skincustom[2] = {
-	name = "Skin 1",
-	skingroup = 1,
+ITEM.skincustom[5] = {
+	name = "Skin 4",
+	skingroup = 4,
 }
+-- ITEM.skincustom[6] = {
+-- 	name = "Skin 5",
+-- 	skingroup = 5,
+-- }
+-- ITEM.skincustom[7] = {
+-- 	name = "Skin 6",
+-- 	skingroup = 6,
+-- }
+-- ITEM.skincustom[8] = {
+-- 	name = "Skin 7",
+-- 	skingroup = 7,
+-- }
+-- ITEM.skincustom[9] = {
+-- 	name = "Skin 8",
+-- 	skingroup = 8,
+-- }
+-- ITEM.skincustom[10] = {
+-- 	name = "Skin 9",
+-- 	skingroup = 9,
+-- }
+-- ITEM.skincustom[11] = {
+-- 	name = "Skin 10",
+-- 	skingroup = 10,
+-- }
+-- ITEM.skincustom[12] = {
+-- 	name = "Skin 11",
+-- 	skingroup = 11,
+-- }
+-- ITEM.skincustom[13] = {
+-- 	name = "Skin 12",
+-- 	skingroup = 12,
+-- }
--- gamemodes/ix_cotz/schema/items/suit/sh_suit_ps5m_fanatic.lua
+++ gamemodes/ix_cotz/schema/items/suit/sh_suit_ps5m_fanatic.lua
@@ -27,11 +27,39 @@ ITEM.carryinc = 9.000
 ITEM.miscslots = 1
 
 
-ITEM.skincustom[1] = {
-	name = "Skin 0",
-	skingroup = 0,
+-- ITEM.skincustom[1] = {
+-- 	name = "Skin 0",
+-- 	skingroup = 0,
+-- }
+-- ITEM.skincustom[2] = {
+-- 	name = "Skin 1",
+-- 	skingroup = 1,
+-- }
+-- ITEM.skincustom[3] = {
+-- 	name = "Skin 2",
+-- 	skingroup = 2,
+-- }
+-- ITEM.skincustom[4] = {
+-- 	name = "Skin 3",
+-- 	skingroup = 3,
+-- }
+-- ITEM.skincustom[5] = {
+-- 	name = "Skin 4",
+-- 	skingroup = 4,
+-- }
+-- ITEM.skincustom[6] = {
+-- 	name = "Skin 5",
+-- 	skingroup = 5,
+-- }
+-- ITEM.skincustom[7] = {
+-- 	name = "Skin 6",
+-- 	skingroup = 6,
+-- }
+ITEM.skincustom[8] = {
+	name = "Skin 7",
+	skingroup = 7,
 }
-ITEM.skincustom[2] = {
-	name = "Skin 1",
-	skingroup = 1,
+ITEM.skincustom[9] = {
+	name = "Skin 8",
+	skingroup = 8,
 }
