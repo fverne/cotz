5f0eb416ddf6ebd81de4f2ab0ba6c18c335e35c7
fverne <frederikverne95@gmail.com>
2024-02-02 20:47:51 +0100

    fixed iconcams on lootboxes

--- gamemodes/ix_cotz/schema/items/kits/sh_kit_reward_tier00_small.lua
+++ gamemodes/ix_cotz/schema/items/kits/sh_kit_reward_tier00_small.lua
@@ -8,9 +8,9 @@ ITEM.flag = "A"
 
 ITEM.exRender = true
 ITEM.iconCam = {
-	pos = Vector(277.58795166016, 232.89892578125, 177.14739990234),
-	ang = Angle(25, 220, 0),
-	fov = 5.0663454801178,
+	pos = Vector(0, -0.68, 200),
+	ang = Angle(449.8, 90.22, 0),
+	fov = 13.55
 }
 
 ITEM.items = {
