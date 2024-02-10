0ce23f6ebeb111fd3862b363127d5b2e58c27eb8
fverne <frederikverne95@gmail.com>
2024-02-10 14:10:20 +0100

    fixed some npc templates

--- gamemodes/ix_cotz/plugins/vendor_adv/libs/sh_npctemplates.lua
+++ gamemodes/ix_cotz/plugins/vendor_adv/libs/sh_npctemplates.lua
@@ -96,8 +96,9 @@ ix.npctemplates.templates["tutorialnpc"] = {
 		["headlamp"] 	= { nil, 3, SELLANDBUY, 3, 1, 1 },
 		["geigercounter"] 	= { nil, 5, SELLANDBUY, 5, 1, 1 },
 
-		["mask_halfmask"] 	= { 5000, 10, SELLONLY, 10, 1, 10 }, -- expensive on purpose
-		["mask_gp5"] 	= { 10000, 10, SELLONLY, 10, 1, 10 }, -- expensive on purpose
+		["mask_cloth"] = {nil, 10, SELLONLY, 10, 1, 10 },
+		["mask_halfmask"] 	= { 15000, 10, SELLONLY, 10, 1, 10 }, -- expensive on purpose
+		["mask_gp5"] 	= { 40000, 10, SELLONLY, 10, 1, 10 }, -- expensive on purpose
 
 		["medic_bandage_1"] = { nil, 12, SELLANDBUY, 12, 1, 1 },
 		["medic_bandage_2"] = { nil, 6, SELLANDBUY, 6, 1, 1 },
