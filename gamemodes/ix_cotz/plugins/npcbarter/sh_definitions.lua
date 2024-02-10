349dbf5687fe5387c1832b50fc68cec37c310b8e
fverne <frederikverne95@gmail.com>
2024-02-10 14:10:12 +0100

    removed debug npcbarters

--- gamemodes/ix_cotz/plugins/npcbarter/sh_definitions.lua
+++ gamemodes/ix_cotz/plugins/npcbarter/sh_definitions.lua
@@ -14,48 +14,48 @@
 	})
 ]]--
 
-ix.npcbarter.Register("'Old Timer'", "testbarter", {
-	name = "Shot for tails",
-	description = "A box of .410 buck for 3 blind dog tails, or 1 pseudodog tail.",
-	defaultActive = true,
+-- ix.npcbarter.Register("'Old Timer'", "testbarter", {
+-- 	name = "Shot for tails",
+-- 	description = "A box of .410 buck for 3 blind dog tails, or 1 pseudodog tail.",
+-- 	defaultActive = true,
 
-	-- Item that will awarded when barter is completed
-	barterItem = {"45acp41"},
+-- 	-- Item that will awarded when barter is completed
+-- 	barterItem = {"45acp41"},
 
-	-- Only one type of item can be required at a time
-	reqItem = {
-		{"part_blinddog", 3},
-		{"part_pseudodog", 1}
-	}
-})
+-- 	-- Only one type of item can be required at a time
+-- 	reqItem = {
+-- 		{"part_blinddog", 3},
+-- 		{"part_pseudodog", 1}
+-- 	}
+-- })
 
-ix.npcbarter.Register("'Old Timer'", "testbarter2", {
-	name = "USSR Surplus",
-	description = "10 shots of 7.62x54 for 4 capacitors.",
-	defaultActive = true,
+-- ix.npcbarter.Register("'Old Timer'", "testbarter2", {
+-- 	name = "USSR Surplus",
+-- 	description = "10 shots of 7.62x54 for 4 capacitors.",
+-- 	defaultActive = true,
 
-	-- Item that will awarded when barter is completed
-	barterItem = {"762x54hp", 1, {["quantity"] = 10}},
+-- 	-- Item that will awarded when barter is completed
+-- 	barterItem = {"762x54hp", 1, {["quantity"] = 10}},
 
-	-- Only one type of item can be required at a time
-	reqItem = {
-		{"value_capacitors", 4},
-	}
-})
+-- 	-- Only one type of item can be required at a time
+-- 	reqItem = {
+-- 		{"value_capacitors", 4},
+-- 	}
+-- })
 
-ix.npcbarter.Register("'Old Timer'", "testbarter3", {
-	name = "Medical Bolts",
-	description = "3 medkits for an anomalous bolt.",
-	defaultActive = true,
+-- ix.npcbarter.Register("'Old Timer'", "testbarter3", {
+-- 	name = "Medical Bolts",
+-- 	description = "3 medkits for an anomalous bolt.",
+-- 	defaultActive = true,
 
-	-- Item that will awarded when barter is completed
-	barterItem = {"medic_medkit_1", 3},
+-- 	-- Item that will awarded when barter is completed
+-- 	barterItem = {"medic_medkit_1", 3},
 
-	-- Only one type of item can be required at a time
-	reqItem = {
-		{"artifact_bolt"},
-	}
-})
+-- 	-- Only one type of item can be required at a time
+-- 	reqItem = {
+-- 		{"artifact_bolt"},
+-- 	}
+-- })
 
 ix.npcbarter.Register("'Old Timer'", "newbiebarter1", {
 	name = "Decomposing Bullets",
