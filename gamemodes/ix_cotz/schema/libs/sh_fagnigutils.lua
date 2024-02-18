ed4ded7636098d81432213e89919d5c471a507e8
fverne <frederikverne95@gmail.com>
2024-02-18 12:10:25 +0100

    made action cancelling 0 seconds

--- gamemodes/ix_cotz/schema/libs/sh_fagnigutils.lua
+++ gamemodes/ix_cotz/schema/libs/sh_fagnigutils.lua
@@ -40,7 +40,7 @@ end
 
 function ix.util.PlayerActionInterrupt(player)
     if (player:GetNetVar("ix_hasBlackScreen")) then
-        player:SetAction("Action Cancelled", 1)
+        player:SetAction("Action Cancelled", 0)
         player:Freeze(false)
         player:SetNetVar("ix_noMenuAllowed", false)
 		player:SetNetVar("ix_hasBlackScreen", nil)
