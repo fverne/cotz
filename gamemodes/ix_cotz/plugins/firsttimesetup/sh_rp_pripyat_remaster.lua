200e9c11fdc84eaaf75685b4d2db10f43e2c4100
fverne <frederikverne95@gmail.com>
2024-02-18 08:33:35 +0100

    added stuff to fts from pripyat

--- gamemodes/ix_cotz/plugins/firsttimesetup/sh_rp_pripyat_remaster.lua
+++ gamemodes/ix_cotz/plugins/firsttimesetup/sh_rp_pripyat_remaster.lua
@@ -30,5 +30,12 @@ PLUGIN.map_presets["rp_pripyat_remaster"] = {
     ["worlditemspawner"] = {
 
     },
+  },
+  entities = {
+    { -- blocks off apartment staircase to bedroll
+      class = "prop_physics",
+      position = Vector(4200.5434570313,8833.66015625,1007.5494995117),
+      angles = Angle(10.264077186584,93.913314819336,-2.4269104003906),
+    },
   }
 }
