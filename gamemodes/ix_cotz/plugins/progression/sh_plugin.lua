eb53b5d5c4b42c5657bfb6d12772bd884133c077
fverne <frederikverne95@gmail.com>
2024-03-29 23:31:25 +0100

    added initial derma

--- gamemodes/ix_cotz/plugins/progression/sh_plugin.lua
+++ gamemodes/ix_cotz/plugins/progression/sh_plugin.lua
@@ -39,3 +39,93 @@ function PLUGIN:PlayerSpawn(client, transition)
 	net.WriteTable(ix.progression.status)
 	net.Send(client)
 end
+
+function PLUGIN:PopulateHelpMenu(tabs)
+    tabs["progression"] = function(container)
+		local title = container:Add("DLabel")
+        title:SetText("Overall Progression")
+        title:SetFont("stalkerregularbigfont")
+        title:Dock(TOP)
+        title:InvalidateLayout(true)
+        title:SetAutoStretchVertical(true)
+
+		local intro = container:Add("DLabel")
+        intro:SetWrap(true)
+        intro:SetFont("stalkerregularfont")
+        intro:SetText("Below is a list of known NPCs and what they need for various progressions. Might be slightly outdated due to the slow networking conditions of the zone.")
+        intro:Dock(TOP)
+        intro:DockMargin(0, 16, 0, 0)
+        intro:SetAutoStretchVertical(true)
+
+		local npcidentifiers = {}
+		local initialNpcs = {
+			"'Old Timer'",
+			"'Technut'",
+			"'Jitters'",
+			"'Cleaner'",
+			"'Quartermaster'",
+			"'Boss'",
+			"'Smartass'",
+		}
+
+		table.Add(npcidentifiers, initialNpcs)
+
+		if ix.progression.IsCompleted("smartass_rfTasks") then
+			local ecologistNpcs = {
+				"'Egghead'",
+				"'Beanstalk'",
+				"'Intern'",
+			}
+			table.Add(npcidentifiers, ecologistNpcs)
+		end
+
+		if ix.progression.IsCompleted("computerDelivery_activateItem") then
+			local pripyatNpcs = {
+				"'Computer'",
+			}
+			table.Add(npcidentifiers, pripyatNpcs)
+		end
+
+		for _, npcidentifier in pairs(npcidentifiers) do
+			local npctitle = container:Add("DLabel")
+			npctitle:SetWrap(true)
+			npctitle:SetFont("stalkerregularbigfont")
+			npctitle:SetText(npcidentifier)
+			npctitle:Dock(TOP)
+			npctitle:DockMargin(0, 16, 0, 0)
+			npctitle:SetAutoStretchVertical(true)
+
+			local activeProgressions = 0
+			for k, v in pairs(ix.progression.GetActiveProgressions(npcidentifier)) do
+				local progdef = ix.progression.definitions[v]
+				local progstatus = ix.progression.GetComplexProgressionValue(v)
+				activeProgressions = activeProgressions + 1
+	
+				local progressiontitle = container:Add("DLabel")
+				progressiontitle:SetWrap(true)
+				progressiontitle:SetFont("stalkerregularfont")
+				progressiontitle:SetText(progdef.name)
+				progressiontitle:Dock(TOP)
+				progressiontitle:DockMargin(0, 16, 0, 0)
+				progressiontitle:SetAutoStretchVertical(true)
+	
+				if(progdef.fnAddComplexProgression)then
+					local progitems 	= progdef.GetItemIds()
+					local missingitems  = {}
+	
+					local progressiondescription = container:Add("DLabel")
+					progressiondescription:SetWrap(true)
+					progressiondescription:SetFont("stalkerregularsmallfont")
+					progressiondescription:SetText(progdef:BuildResponse(progdef.keyNpc, progstatus))
+					progressiondescription:Dock(TOP)
+					progressiondescription:DockMargin(0, 16, 0, 0)
+					progressiondescription:SetAutoStretchVertical(true)
+				end
+			end
+
+			if activeProgressions == 0 then
+				npctitle:Remove()
+			end
+		end
+	end
+end
