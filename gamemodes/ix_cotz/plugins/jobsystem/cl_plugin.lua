7cb56feb5d1ed580d366d86415f997f784cdca4f
fverne <frederikverne95@gmail.com>
2025-12-01 16:03:01 +0100

    UI Scaling overhaul, please report bugs

--- gamemodes/ix_cotz/plugins/jobsystem/cl_plugin.lua
+++ gamemodes/ix_cotz/plugins/jobsystem/cl_plugin.lua
@@ -63,13 +63,13 @@ if(CLIENT) then
 			self.index.description:SetText(string.format(job.name or "%d Unknown", v.numberRec))
 			self.index.description:Dock(TOP)
 			self.index.description:DockMargin(self.index:GetWide()*0.1, self.index:GetTall()*0.05, 0, self.index:GetTall()*0.05)
-			self.index.description:SetFont("ixGenericFont")
+			self.index.description:SetFont("stalkerregularfont2")
 			self.index.description:SetColor(Color(255, 255, 255))
 
 			self.index.description = self.index:Add("DLabel")
 			self.index.description:SetText(string.format("Progress: ".." %d / %d", v.progress, v.numberRec))
 			self.index.description:Dock(TOP)
-			self.index.description:SetFont("ixGenericFont")
+			self.index.description:SetFont("stalkerregularfont2")
 			self.index.description:DockMargin(self.index:GetWide()*0.1, self.index:GetTall()*0.05, 0, self.index:GetTall()*0.05)
 			if v.progress == v.numberRec then
 				self.index.description:SetText("Objective completed.")
@@ -80,7 +80,7 @@ if(CLIENT) then
 			self.index.taskgiver:SetText("Task Giver: "..index)
 			self.index.taskgiver:Dock(TOP)
 			self.index.taskgiver:DockMargin(self.index:GetWide()*0.1, self.index:GetTall()*0.05, 0, 0)
-			self.index.taskgiver:SetFont("ixGenericFont")
+			self.index.taskgiver:SetFont("stalkerregularfont2")
 			self.index.taskgiver:SetColor(Color(255, 255, 255))
 
 			--[[
