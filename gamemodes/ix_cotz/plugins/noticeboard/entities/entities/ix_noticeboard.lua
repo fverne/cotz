e69afc8c5350ad00a7657ac5dbc52233036b85af
fverne <frederikverne95@gmail.com>
2025-12-09 14:23:40 +0100

    potential encyclopedia fix

--- gamemodes/ix_cotz/plugins/noticeboard/entities/entities/ix_noticeboard.lua
+++ gamemodes/ix_cotz/plugins/noticeboard/entities/entities/ix_noticeboard.lua
@@ -11,17 +11,10 @@ if (SERVER) then
 	function ENT:Initialize()
 		self:SetModel("models/hunter/plates/plate1x2.mdl")
 		self:SetSolid(SOLID_VPHYSICS)
-		self:PhysicsInit(SOLID_VPHYSICS)
+		-- self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetUseType(SIMPLE_USE)
 		self:SetMaterial("models/props/CS_militia/roofbeams02")
 		self:AddProps()
-
-		local physObj = self:GetPhysicsObject()
-
-		if (IsValid(physObj)) then
-			physObj:EnableMotion(true)
-			physObj:Wake()
-		end
 	end
 
     function ENT:AddProps()
