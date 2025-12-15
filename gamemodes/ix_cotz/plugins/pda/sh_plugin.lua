a86dcdff87f2f62bb1578a7bd08a0a2ae5407e50
fverne <frederikverne95@gmail.com>
2025-12-15 22:23:53 +0100

    fixed mixed pda colours

--- gamemodes/ix_cotz/plugins/pda/sh_plugin.lua
+++ gamemodes/ix_cotz/plugins/pda/sh_plugin.lua
@@ -149,7 +149,7 @@ ix.chat.Register("npcpdainternal", {
 		return true
 	end,
 	OnChatAdd = function(self, speaker, text, bAnonymous, data)
-		chat.AddText(Color(180,61,61), "[GPDA-"..data.name.."] ", Color(0,241,255), icon, ": "..data.message)
+		chat.AddText(Color(180,61,61), "[GPDA-"..data.name.."] ", Color(255,255,255), icon, ": "..data.message)
 	end,
 	prefix = {},
 	CanHear = function(self, speaker, listener)
