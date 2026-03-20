fe5535c00202f96183611bfcc203db14029113f4
El Diablo <thiefhideout@yandex.ru>
2026-03-20 10:59:48 +0300

    Removed old commented code

--- lua/entities/anom_par/init.lua
+++ lua/entities/anom_par/init.lua
@@ -38,40 +38,6 @@ function ENT:Initialize()
 	end
 end
 
--- function ENT:StartTouch(ent)
--- 	if not self.Active then return end
--- 	timer.Create("par_activated_once"..self:EntIndex(), 0.01, 1, function()
--- 		self:EmitSound("par_blast");
--- 		ParticleEffectAttach( "par_anomaly", PATTACH_ABSORIGIN_FOLLOW, self, 1 )
--- 		if IsValid(ent) and ent:IsRagdoll() and ent:GetNetVar("player") == nil then
--- 			local bodyexplodesounds = {"anomaly/anomaly_body_tear_1.wav", "anomaly/anomaly_body_tear_2.wav"}
--- 			ent:EmitSound(table.Random(bodyexplodesounds),100,98,1,CHAN_AUTO)
--- 			ParticleEffect("wick_gore_bloodsplash_new" , ent:GetPos(), Angle( 0, 0, 0 ) )
--- 			ent:Remove()
--- 		elseif ent.ixItemID and ix.item.instances[ent.ixItemID].isWeapon then
--- 			ent:Remove()
--- 		end
--- 		--util.BlastDamage( self, self, self:GetPos(), 100, 50)
--- 		--ent:TakeDamage(50, self, self)
--- 	end)
--- 	timer.Create("par_activated"..self:EntIndex(), 12, 1, function()
--- 		self:StopSound("par_blast");
--- 		self.Active = true
--- 		ParticleEffect("jarka_inactive", self:GetPos(), Angle(0,0,0), self)
--- 		self:StopParticles()
--- 	end)
--- 	self.Active = false
--- end
-
--- function ENT:Touch(activator)
--- 	--util.BlastDamage( self, self, self:GetPos(), 100, 2)
--- 	if self.cooldown < CurTime() then
--- 		activator:TakeDamage(30, self, self)
--- 		self.cooldown = CurTime() + 0.5
--- 	end
-	
--- end
-
 function ENT:Touch(ent)
 	if not self.Active then return end
 	if timer.Exists(self:EntIndex().."_par_activated_"..ent:EntIndex()) then return end
